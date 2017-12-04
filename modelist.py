46633e0047921dad1a9cd9480efe6132aefe1907
mohitcek <31354965+mohitcek@users.noreply.github.com>
2017-12-04 11:11:08 -0500

    Doc Strings: normpdf, mvnpdf, marginal

--- modelist.py
+++ modelist.py
@@ -135,11 +135,27 @@ def model_reliability(u, Type):
 ########################################################################################################################
 
 def normpdf(x):
+    """ Normal density function used to generate samples using Metropolis-Hastings Algorithm
+
+    `..math:: f(x) = \frac{1}{(2*\pi*\sigma)^(1/2)}*exp(-\frac{1}{2}*(\frac{x-\mu}{\sigma})^2)
+
+    """
     return stats.norm.pdf(x, 0, 1)
 
 
 def mvnpdf(x, dim):
+    """ Multivariate normal density function used to generate samples using Metropolis-Hastings Algorithm
+
+    `..math:: f(x_{1},...,x_{k}) = \frac{1}{((2*\pi)^{k}*\Sigma)^(1/2)}*exp(-\frac{1}{2}*(x-\mu)^{T}*\Sigma^{-1}*(x-\mu))
+
+    """
     return stats.multivariate_normal.pdf(x, mean=np.zeros(dim), cov=np.identity(dim))
 
+
 def marginal(x, mp):
+    """ Marginal target density used to generate samples using Modified Metropolis-Hastings Algorithm
+
+    `..math:: f(x) = \frac{1}{(2*\pi*\sigma)^(1/2)}*exp(-\frac{1}{2}*(\frac{x-\mu}{\sigma})^2)
+
+    """
     return stats.norm.pdf(x, mp[0], mp[1])
