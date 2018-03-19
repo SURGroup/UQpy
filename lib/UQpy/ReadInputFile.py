919e2100f9d949d80e794d1cc91aa6692e74972a
dgiovanis <30946950+dgiovanis@users.noreply.github.com>
2018-03-19 12:05:27 -0400

    Add files via upload

---
+++ lib/UQpy/ReadInputFile.py
@@ -0,0 +1,196 @@
+import numpy as np
+
+
+def readfile(filename):
+    lines_ = []
+    mydict = {}
+    count = -1
+    for line in open(filename):
+        rec = line.strip()
+        count = count + 1
+        if rec.startswith('#'):
+            lines_.append(count)
+
+    f = open(filename)
+    lines = f.readlines()
+
+    for i in range(len(lines_)):
+        title = lines[lines_[i]][1:-1]
+        ################################################################################################################
+        # General parameters
+        if title == 'method':
+            mydict[title] = lines[lines_[i]+1][:-1]
+            print()
+        elif title == 'distribution type':
+            dist = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    dist.append(lines[lines_[i] + j + 1][:-1])
+                    j = j + 1
+            mydict[title] = dist
+        elif title == 'names of parameters':
+            names = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    names.append(lines[lines_[i] + j + 1][:-1])
+                    j = j + 1
+            mydict[title] = names
+        elif title == 'distribution parameters':
+            params = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    x = lines[lines_[i] + j + 1]
+                    params.append(np.float32(x.split(" ")))
+                    j = j + 1
+            mydict[title] = params
+        elif title == 'number of samples':
+            mydict[title] = int(lines[lines_[i] + 1][:-1])
+        elif title == 'number of parameters':
+            mydict[title] = int(lines[lines_[i] + 1][:-1])
+        ################################################################################################################
+        # Latin Hypercube parameters
+        elif title == 'criterion':
+            mydict[title] = lines[lines_[i] + 1][:-1]
+        elif title == 'distance':
+            mydict[title] = lines[lines_[i] + 1][:-1]
+        elif title == 'iterations':
+            mydict[title] = lines[lines_[i] + 1][:-1]
+        ################################################################################################################
+        #  partially stratified sampling
+        elif title == 'design':
+            pss_design = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    x = lines[lines_[i] + j + 1]
+                    pss_design.append(int(lines[lines_[i] + j + 1][:-1]))
+                    j = j + 1
+            mydict[title] = pss_design
+        elif title == 'strata':
+            pss_strata = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    pss_strata.append(int(lines[lines_[i] + j + 1][:-1]))
+                    j = j + 1
+            mydict[title] = pss_strata
+        ################################################################################################################
+        #  stratified sampling
+        elif title == 'design':
+            sts_design = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    sts_design.append(int(lines[lines_[i] + j + 1][:-1]))
+                    j = j + 1
+            mydict[title] = sts_design
+            print(sts_design)
+        ################################################################################################################
+        # Markov Chain Monte Carlo simulation
+        elif title == 'algorithm':
+            mydict[title] = lines[lines_[i] + 1][:-1]
+        elif title == 'proposal distribution':
+            mydict[title] = lines[lines_[i] + 1][:-1]
+        elif title == 'proposal distribution width':
+            mydict[title] = int(lines[lines_[i] + 1][:-1])
+        elif title == 'target distribution':
+            mydict[title] = lines[lines_[i] + 1][:-1]
+        elif title == 'skip':
+            mydict[title] = int(lines[lines_[i] + 1][:-1])
+        elif title == 'target distribution parameters':
+            x = lines[lines_[i] + 1]
+            target_params = np.float32(x.split(" "))
+            mydict[title] = target_params
+        elif title == 'seed':
+            seed = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    x = lines[lines_[i] + j + 1]
+                    seed.append(np.float32(x.split(" ")))
+                    j = j + 1
+            mydict[title] = seed
+        ################################################################################################################
+        # Stochastic Reduced Order Model
+        elif title == 'SROM':
+            mydict[title] = lines[lines_[i] + 1][:-1]
+        elif title == 'moments':
+            seed = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    x = lines[lines_[i] + j + 1][:-1]
+                    seed.append(np.float32(x.split(" ")))
+                    j = j + 1
+            mydict[title] = seed
+        elif title == 'error function weights':
+            seed = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    x = lines[lines_[i] + j + 1][:-1]
+                    seed.append(np.float32(x.split(" ")))
+                    j = j + 1
+            mydict[title] = seed
+        elif title == 'sample weights':
+            seed = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    x = lines[lines_[i] + j + 1][:-1]
+                    seed.append(np.float32(x.split(" ")))
+                    j = j + 1
+            mydict[title] = seed
+        elif title == 'properties to match':
+            seed = []
+            j = 0
+            while j >= 0:
+                testline = lines[lines_[i] + j + 1].strip()
+                if not testline:
+                    break
+                else:
+                    x = lines[lines_[i] + j + 1][:-1]
+                    seed.append(np.float32(x.split(" ")))
+                    j = j + 1
+            mydict[title] = seed
+        ################################################################################################################
+        # ADD ANY NEW METHOD HERE
+
+        ################################################################################################################
+        # ADD ANY NEW METHOD HERE
+
+
+    return mydict
---
+++ lib/UQpy/UQpy_cmd.py
@@ -0,0 +1,65 @@
+import os
+import sys
+from argparse import ArgumentParser
+import numpy as np
+
+UQpy_commandLine = \
+UQpy_commandLine_Usage = """python UQpy.py --{options}"""
+
+if __name__ == '__main__':
+
+    UQpymainDir = os.path.realpath(__file__)
+    UQpydirectory = os.path.dirname(UQpymainDir)
+
+    parser = ArgumentParser(description=UQpy_commandLine,
+                            usage=UQpy_commandLine_Usage)
+
+    parser.add_argument("--dir", dest="Model_directory", action="store",
+                        default=None, help="Specify the location of the model's directory.")
+
+    parser.add_argument("--input", default=None,
+                        dest="Input_Shell_Script", action="store",
+                        help="Specify the name of the shell script  *.sh used to transform  the output of UQpy"
+                             " (UQpyOut_*.txt file) into the appropriate model input file ")
+
+    parser.add_argument("--output", default=None,
+                        dest="Output_Shell_Script", action="store",
+                        help="Specify the name of the shell script  *.sh used to transform  the output of the model"
+                             " into the appropriate UQpy input file (UQpyInp_*.txt) ")
+
+    parser.add_argument("--model", dest="Solver", action="store",
+                        default=None, help="Specify the name of the shell script used for running the model")
+
+    parser.add_argument("--CPUs", dest="CPUs", action="store",
+                        default=0, type=int, help="Number of local cores to be used for the analysis")
+
+    parser.add_argument("--ClusterNodes", dest="nodes", action="store", type=int,
+                        default=1, help="Number of nodes to distribute the model evaluations in "
+                        "case of a cluster (Not Available).")
+
+    args, unknown = parser.parse_known_args()
+
+    if len(unknown) > 0:
+        print("Warning:  The following unrecognized arguments were ignored:")
+        print(unknown)
+
+    if len(sys.argv) > 1:
+        if args.Model_directory is None:
+            print("Error: A model directory needs to be specified")
+            sys.exit()
+
+        if args.Solver is not None:
+            if args.Input_Shell_Script is None:
+                print("Error: Shell scripts for communication between UQpy and the model are required. Type --help"
+                      "for more information")
+                sys.exit()
+
+            elif args.Output_Shell_Script is None:
+                print("Error: Shell scripts for communication between UQpy and the model are required. Type --help"
+                      "for more information")
+                sys.exit()
+        
+        from lib.UQpy.UQpyModules import RunCommandLine
+        RunCommandLine(args)
+
+  
