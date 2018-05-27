9fe08628e4b4aa185cb3629142d8abdd7a396909
Michael Shields <michael.shields@jhu.edu>
2018-05-27 18:57:37 -0400

    Removed Examples, Templates, and Documentation from Github

--- Templates/UQpy_Output.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Don not modify
-filename="UQpy_eval_$1.txt"
-touch "$filename"
-
-
-# Chnage ONLY the name "solution_" according to the name of the model's output
-cat "solution_$1.txt" >> "$filename"
