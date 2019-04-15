fc6c8f30cfa732d50cddf4d74e8962de5ca989d0
Dimitris Giovanis <30946950+dgiovanis@users.noreply.github.com>
2019-04-15 13:49:50 -0400

    Update setup.py
    -Update available platforms
    -Remove unnecessary install requirements

--- setup.py
+++ setup.py
@@ -9,12 +9,16 @@ setup(
     authors="Michael D. Shields, Dimitris G. Giovanis",
     author_emails="michael.shields@jhu.edu, dgiovan1@jhu.edu",
     license='MIT',
-    platforms='OSX',
+    platforms=[
+        'OSX',
+        'Windows',
+        'Linux'
+    ],
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data={"":["*.pdf"]},
     install_requires=[
-        "numpy", "scipy", "chaospy", "pyDOE", "scikit-learn", 'fire'
+        "numpy", "scipy", "matplotlib", "scikit-learn", 'fire'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
