b061336f2637479280a038ccf0fa661392e77c32
dgiov16 <moonwalkerd@hotmail.com>
2019-10-28 21:02:24 -0400

    Modify Transformations module (v3) and update examples Remove redundant functions from Utilities module

--- src/UQpy/Transformations.py
+++ src/UQpy/Transformations.py
@@ -19,10 +19,8 @@
 
 from UQpy.Utilities import *
 from UQpy.Distributions import *
+from scipy.linalg import cholesky
 
-
-# Authors: Dimitris G.Giovanis
-# Last Modified: 10/26/19 by Dimitris G. Giovanis
 
 class Dependence:
     """
@@ -45,11 +43,14 @@ class Dependence:
 
     """
 
-    def __init__(self, action=None, input_samples=None, corr_norm=None):
+    # Authors: Dimitris G.Giovanis
+    # Last Modified: 10/28/19 by Dimitris G. Giovanis
+
+    def __init__(self, input_samples=None, corr_norm=None):
         self.flag = 0  # a flag to handle the case where the correlation matrix is the identity matrix
         self.corr_norm = corr_norm
 
-        """ If the input samples are of type object or ndarray"""
+        """ If the input samples are of type object or array"""
         if not isinstance(input_samples, np.ndarray):
             _dict = {**input_samples.__dict__}
             for k, v in _dict.items():
@@ -76,143 +77,23 @@ class Dependence:
             print("The provided correlation matrix is the identity. No action will be performed")
             self.flag = 1
 
-        self.action = action
+    def correlate(self):
+        c = cholesky(self.corr_norm, lower=True)
+        if self.flag == 0:
+            self.samples = np.dot(c, self.samples.copy().T).T
+        if self.flag == 1:
+            self.samples = self.samples.copy()
 
-    def run(self):
-        if self.action == 'Correlate' and self.flag == 0:
-            self.samples = run_corr(self.samples.copy(), self.corr_norm)
-        if self.action == 'Decorrelate' and self.flag == 0:
-            self.samples = run_decorr(self.samples.copy(), self.corr_norm)
+    def decorrelate(self):
+        c = cholesky(self.corr_norm, lower=True)
+        if self.flag == 0:
+            self.samples = np.dot(np.linalg.inv(c), self.samples.copy().T).T
         if self.flag == 1:
             self.samples = self.samples.copy()
 
 
-class InvNataf:
-    """
-        Description:
-
-            A class to perform the inverse Nataf transformation, i.e. From N(0, 1) to a user-defined distribution.
-
-        Input:
-            :param input_samples: An object of SampleMethods class or an array containing N(0,1) samples.
-            :type input_samples: object or ndarray
-
-            :param dist_name: A list containing the names of the distributions of the random variables.
-                              Distribution names must match those in the Distributions module.
-                              If the distribution does not match one from the Distributions module,the user must provide
-                              custom_dist.py.
-                              The length of the string must be 1 (if all distributions are the same) or equal to
-                              dimension.
-            :type dist_name: string list
-
-            :param dist_params: Parameters of the distribution.
-                                Parameters for each random variable are defined as ndarrays
-                                Each item in the list, dist_params[i], specifies the parameters for the corresponding
-                                distribution, dist[i].
-            :type dist_params: list
-
-            :param corr_norm: The correlation matrix of the random variables in the standard normal space
-            :type corr_norm: ndarray
-
-            param: distribution: An object list containing the distributions of the random variables.
-                                 Each item in the list is an object of the Distribution class (see Distributions.py).
-                                 The list has length equal to dimension.
-            :type distribution: list
-
-        Output:
-            :return: InvNataf.corr: The distorted correlation matrix of the random variables in the standard space;
-            :rtype: InvNataf.corr: ndarray
-
-            :return: InvNataf.samplesN01: An array of N(0,1) samples;
-            :rtype: InvNataf.corr: ndarray
-
-            :return: InvNataf.samples: An array of samples following the prescribed distributions;
-            :rtype: InvNataf.corr: ndarray
-
-            :return: InvNataf.jacobian: An array containing the Jacobian of the transformation.
-            :rtype: InvNataf.jacobian: ndarray
-
-    """
-
-    # Authors: Dimitris G. Giovanis
-    # Last Modified: 10/26/2019 by Dimitris G. Giovanis
-
-    def __init__(self, input_samples=None, corr_norm=None, dist_name=None, dist_params=None, dimension=None):
-        self.dist_nameNG = dist_name
-        self.dist_paramsNG = dist_params
-
-        if input_samples is not None:
-
-            if isinstance(input_samples, np.ndarray):
-                self.samplesN01 = input_samples
-                self.dist_name = 'normal'
-                self.dist_params = [0, 1]
-                self.corr_norm = corr_norm
-                self.dimension = dimension
-
-            else:
-                _dict = {**input_samples.__dict__}
-
-                for k, v in _dict.items():
-                    setattr(self, k, v)
-                if not hasattr(self, 'dist_name'):
-                    self.dist_name = 'normal'
-                if not hasattr(self, 'dist_params'):
-                    self.dist_params = [0, 1]
-                if not hasattr(self, 'corr_norm'):
-                    self.corr_norm = corr_norm
-
-                self.samplesN01 = self.samples.copy()
-
-        if input_samples is None:
-
-            self.dist_name = 'normal'
-            self.dist_params = [0, 1]
-            self.corr_norm = corr_norm
-            self.samplesN01 = input_samples
-
-            try:
-                self.corr_norm is None
-            except:
-                raise RuntimeError("UQpy: To perform the inverse Nataf transformation without samples "
-                                   " a correlation matrix must be provided.")
-
-        if self.dist_nameNG is None or self.dist_paramsNG is None:
-            raise RuntimeError("In order to use class 'InvNataf' the distributions and their parameters must"
-                               "be provided.")
-        if dimension is None:
-            self.dimension = len(self.dist_nameNG)
-        else:
-            self.dimension = dimension
-
-        # Ensure the dimensions of dist_name are consistent
-        if type(self.dist_nameNG).__name__ != 'list':
-            self.dist_nameNG = [self.dist_nameNG]
-        if len(self.dist_nameNG) == 1 and self.dimension != 1:
-            self.dist_nameNG = self.dist_nameNG * self.dimension
-
-        # Ensure that dist_params is a list of length dimension
-        if type(self.dist_paramsNG).__name__ != 'list':
-            self.dist_paramsNG = [self.dist_params]
-        if len(self.dist_paramsNG) == 1 and self.dimension != 1:
-            self.dist_paramsNG = self.dist_paramsNG * self.dimension
-
-        self.distribution = [None] * self.dimension
-        for j in range(len(self.dist_nameNG)):
-            self.distribution[j] = Distribution(self.dist_nameNG[j])
-
-    def run(self):
-        ident_ = np.linalg.norm(self.corr_norm - np.identity(n=self.dimension)) > 10 ** (-8)
-        if self.corr_norm is not None or ident_ is True or self.samplesN01 is None:
-            self.corr = correlation_distortion(self.distribution, self.dist_paramsNG, self.corr_norm)
-        else:
-            self.corr = self.corr_norm
-        if self.samplesN01 is not None:
-            self.samples, self.jacobian = transform_g_to_ng(self.corr_norm, self.distribution, self.dist_paramsNG,
-                                                            self.samplesN01)
-
-
 class Nataf:
+
     """
         Description:
             A class to perform the Nataf transformation of samples from a user-defined distribution to N(0, 1).
@@ -262,14 +143,12 @@ class Nataf:
             :return: Nataf.jacobian: An array containing the Jacobian of the transformation.
             :rtype: Nataf.jacobian: ndarray
     """
+    # Authors: Dimitris G. Giovanis
+    # Last Modified: 10/28/2019 by Dimitris G. Giovanis
 
-    # Authors: Dimitris G.Giovanis
-    # Last Modified: 10/26/2019 by Dimitris G. Giovanis
+    def __init__(self, input_samples=None, corr_norm=None, corr=None, dist_name=None, dist_params=None,
+                 dimension=None, beta=None, itam_error1=None, itam_error2=None):
 
-    def __init__(self, input_samples=None, dimension=None, corr=None, dist_name=None, dist_params=None, beta=None,
-                 itam_error1=None, itam_error2=None):
-        self.dist_name = 'normal'
-        self.dist_params = [0, 1]
         self.beta = beta
         self.itam_error1 = itam_error1
         self.itam_error2 = itam_error2
@@ -277,70 +156,98 @@ class Nataf:
         if input_samples is not None:
 
             if isinstance(input_samples, np.ndarray):
-                self.samplesNG = input_samples
-                self.dist_nameNG = dist_name
-                self.dist_paramsNG = dist_params
+                self.input_samples = input_samples
+                self.dist_name = dist_name
+                self.dist_params = dist_params
+                self.corr_norm = corr_norm
                 self.corr = corr
                 self.dimension = dimension
 
             else:
                 _dict = {**input_samples.__dict__}
-
                 for k, v in _dict.items():
                     setattr(self, k, v)
-                if not hasattr(self, 'dist_name'):
-                    self.dist_name = 'normal'
-                if not hasattr(self, 'dist_params'):
-                    self.dist_params = [0, 1]
                 if not hasattr(self, 'corr_norm'):
                     self.corr_norm = corr_norm
+                if not hasattr(self, 'corr'):
+                    self.corr = corr
+                self.input_samples = self.samples.copy()
 
-                self.samplesNG = self.samples.copy()
+                # Check whether the input samples are all standard normal:
+                count = 0
+                for i in range(len(self.dist_name)):
+                    if self.dist_name[i].lower() == 'normal' and self.dist_params[i] == [0, 1]:
+                        count = count + 1
+
+                if count == len(self.dist_name):
+                    self.dist_name = dist_name
+                    self.dist_params = dist_params
 
         if input_samples is None:
-
-            self.dist_nameNG = dist_name
-            self.dist_paramsNG = dist_params
+            self.input_samples = input_samples
+            self.dist_name = dist_name
+            self.dist_params = dist_params
+            self.corr_norm = corr_norm
             self.corr = corr
-            self.samplesNG = input_samples
+            self.dimension = dimension
 
-            try:
-                self.corr is None
-            except:
-                raise RuntimeError("UQpy: To perform the inverse Nataf transformation without samples "
-                                   " a correlation matrix must be provided.")
-
-        if self.dist_nameNG is None or self.dist_paramsNG is None:
-            raise RuntimeError("In order to use class 'InvNataf' the distributions and their parameters must"
+        if self.dist_name is None or self.dist_params is None:
+            raise RuntimeError("In order to use class 'Nataf' the distributions and their parameters must"
                                "be provided.")
         if dimension is None:
-            self.dimension = len(self.dist_nameNG)
+            self.dimension = len(self.dist_name)
         else:
             self.dimension = dimension
 
         # Ensure the dimensions of dist_name are consistent
-        if type(self.dist_nameNG).__name__ != 'list':
-            self.dist_nameNG = [self.dist_nameNG]
-        if len(self.dist_nameNG) == 1 and self.dimension != 1:
-            self.dist_nameNG = self.dist_nameNG * self.dimension
+        if type(self.dist_name).__name__ != 'list':
+            self.dist_name = [self.dist_name]
+        if len(self.dist_name) == 1 and self.dimension != 1:
+            self.dist_name = self.dist_name * self.dimension
 
         # Ensure that dist_params is a list of length dimension
-        if type(self.dist_paramsNG).__name__ != 'list':
-            self.dist_paramsNG = [self.dist_params]
-        if len(self.dist_paramsNG) == 1 and self.dimension != 1:
-            self.dist_paramsNG = self.dist_paramsNG * self.dimension
+        if type(self.dist_params).__name__ != 'list':
+            self.dist_params = [self.dist_params]
+        if len(self.dist_params) == 1 and self.dimension != 1:
+            self.dist_params = self.dist_params * self.dimension
 
         self.distribution = [None] * self.dimension
-        for j in range(len(self.dist_nameNG)):
-            self.distribution[j] = Distribution(self.dist_nameNG[j])
+        for j in range(len(self.dist_name)):
+            self.distribution[j] = Distribution(self.dist_name[j])
 
-    def run(self):
-        ident_ = np.linalg.norm(self.corr - np.identity(n=self.dimension)) > 10 ** (-8)
-        if self.corr is not None or ident_ is True or self.samplesNG is None:
-            self.corr_norm = itam(self.distribution, self.dist_paramsNG, self.corr, self.beta, self.itam_error1,
-                                  self.itam_error2)
+    def inverse(self):
+        samples_n01 = self.input_samples.copy()
+        if self.corr_norm is None:
+            self.corr_norm = np.eye(self.dimension)
+        self.samples, self.jacobian = transform_g_to_ng(self.corr_norm, self.distribution, self.dist_params,
+                                                        samples_n01)
+
+    def transform(self):
+        samples_ng = self.input_samples.copy()
+        if self.corr is None:
+            self.corr = np.eye(self.dimension)
+        self.samples, self.jacobian = transform_ng_to_g(self.corr, self.distribution, self.dist_params,
+                                                        samples_ng)
+
+    def correlation_distortion_inv(self):
+        if self.corr_norm is not None:
+            ident_ = np.linalg.norm(self.corr_norm - np.identity(n=self.dimension)) > 10 ** (-8)
+            if ident_:
+                self.corr = correlation_distortion(self.distribution, self.dist_params, self.corr_norm)
+            else:
+                self.corr = self.corr_norm.copy()
         else:
-            self.corr_norm = self.corr
-        if self.samplesNG is not None:
-            self.samples, self.jacobian = transform_ng_to_g(self.corr_norm, self.distribution, self.dist_paramsNG,
-                                                            self.samplesNG)
+            raise RuntimeError("In order to calculate the correlation distortion"
+                               "a correlation matrix is required.")
+
+    def correlation_distortion(self):
+        if self.corr is not None:
+            i_check_ = np.linalg.norm(self.corr - np.identity(n=self.dimension)) > 10 ** (-8)
+            if i_check_:
+                self.corr_norm = itam(self.distribution, self.dist_params, self.corr, self.beta, self.itam_error1,
+                                      self.itam_error2)
+            else:
+                self.corr_norm = self.corr.copy()
+        else:
+            raise RuntimeError("In order to calculate the correlation distortion"
+                               "a correlation matrix is required.")
