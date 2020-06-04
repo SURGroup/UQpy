6761be852da411184071a38e687cfeb704452de2
AudOlivier <ado2111@columbia.edu>
2020-06-04 10:41:57 -0400

    corrected typo in Cauchy distribution

--- src/UQpy/Distributions.py
+++ src/UQpy/Distributions.py
@@ -356,7 +356,7 @@ class Cauchy(DistributionContinuous1D):
     * ``cdf``, ``pdf``, ``log_pdf``, ``icdf``, ``rvs``, ``moments``, ``fit``
     """
     def __init__(self, loc=0., scale=1.):
-        super().__init__(ploc=loc, scale=scale, order_params=('loc', 'scale'))
+        super().__init__(loc=loc, scale=scale, order_params=('loc', 'scale'))
         self._construct_from_scipy(scipy_name=stats.cauchy)
 
 
