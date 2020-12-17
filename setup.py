b35153931d462f98104a02795fe7714184af5c07
Michael Shields <michael.shields@jhu.edu>
2020-12-17 11:32:03 -0500

    Changed setup.py for V3.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='UQpy',
-    version="3.0.1",
+    version="3.0.2",
     url='https://github.com/SURGroup/UQpy',
     description="UQpy is a general purpose toolbox for Uncertainty Quantification",
     author="Michael D. Shields, Dimitris G. Giovanis, Audrey Olivier, Aakash Bangalore-Satish, Mohit Chauhan, "
