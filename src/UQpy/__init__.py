e7d68703e6d68c13098fc15017268761387b784d
dgiovanis <dgiovan1@jhu.edu>
2021-01-28 11:01:07 -0500

    Restructuring :construction: of UQpy

--- src/UQpy/__init__.py
+++ src/UQpy/__init__.py
@@ -5,23 +5,26 @@ Uncertainty Quantification with Python
 
 import pkg_resources
 
-import UQpy.DimensionReduction
 import UQpy.Distributions
+import UQpy.SampleMethods
+import UQpy.DimensionReduction
 import UQpy.Inference
 import UQpy.Reliability
 import UQpy.RunModel
 import UQpy.SampleMethods
-import UQpy.StochasticProcess
+#import UQpy.StochasticProcess
 import UQpy.Surrogates
 import UQpy.Transformations
 import UQpy.Utilities
+
+
 from UQpy.DimensionReduction import *
 from UQpy.Distributions import *
 from UQpy.Inference import *
 from UQpy.Reliability import *
 from UQpy.RunModel import *
 from UQpy.SampleMethods import *
-from UQpy.StochasticProcess import *
+#from UQpy.StochasticProcess import *
 from UQpy.Surrogates import *
 from UQpy.Transformations import *
 from UQpy.Utilities import *
