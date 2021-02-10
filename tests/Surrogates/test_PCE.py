efd1ec920afff9f7b10636dfc0a2c85b7f5bb521
dgiovanis <dgiovan1@jhu.edu>
2021-02-09 19:40:35 -0500

    fix tests structure

--- tests/Surrogates/test_PCE.py
+++ tests/Surrogates/test_PCE.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.append('../../src/UQpy/')
-from Distributions import *
-from Surrogates import *
+import UQpy
+from UQpy.Distributions import *
+from UQpy.Surrogates import *
 import numpy as np
 
 np.random.seed(1)
