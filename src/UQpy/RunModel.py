952b6763ac6860265e469aff5249767812db5ab3
lohitv <lohitvandanapu@gmail.com>
2021-05-18 14:12:20 -0400

    added more print statements to debug

--- src/UQpy/RunModel.py
+++ src/UQpy/RunModel.py
@@ -460,6 +460,9 @@ class RunModel:
 
         # Check if there is a template input file or not and execute the appropriate function
         if self.input_template is not None:  # If there is a template input file
+            print("User requested print statement")
+            print(os.getcwd())
+            print(os.listdir())
             print(os.path.isfile(self.input_template))
             print(os.access(self.input_template, os.R_OK))
             # Check if it is a file and is readable
--- tests/RunModel/test_RunModel.py
+++ tests/RunModel/test_RunModel.py
@@ -10,6 +10,7 @@ import numpy as np
 
 d = Normal(loc=0, scale=1)
 x_mcs = MCS(dist_object=[d, d, d], nsamples=5, random_state=1234)
+temp = False
 
 
 def test_div_zero():
@@ -41,42 +42,42 @@ def test_samples():
 
 
 def test_python_serial_workflow_class_vectorized():
-    model_python_serial_class = RunModel(ntasks=1, model_script='python_model.py', model_object_name='SumRVs', verbose=True)
+    model_python_serial_class = RunModel(ntasks=1, model_script='python_model.py', model_object_name='SumRVs', verbose=temp)
     model_python_serial_class.run(samples=x_mcs.samples)
     assert model_python_serial_class.qoi_list == [2.5086338287600496, 0.6605587413536298, 1.7495075921211787,
                                                   -2.3182103441722544, -3.297351053358514]
 
 
 def test_python_serial_workflow_class():
-    model_python_serial_class = RunModel(ntasks=1, model_script='python_model.py', model_object_name='SumRVs', vec=False, verbose=True)
+    model_python_serial_class = RunModel(ntasks=1, model_script='python_model.py', model_object_name='SumRVs', vec=False, verbose=temp)
     model_python_serial_class.run(samples=x_mcs.samples)
     assert model_python_serial_class.qoi_list == [2.5086338287600496, 0.6605587413536298, 1.7495075921211787,
                                                   -2.3182103441722544, -3.297351053358514]
 
 
 def test_python_serial_workflow_function_vectorized():
-    model_python_serial_function = RunModel(ntasks=1, model_script='python_model.py', model_object_name='sum_rvs', verbose=True)
+    model_python_serial_function = RunModel(ntasks=1, model_script='python_model.py', model_object_name='sum_rvs', verbose=temp)
     model_python_serial_function.run(samples=x_mcs.samples)
     assert model_python_serial_function.qoi_list == [2.5086338287600496, 0.6605587413536298, 1.7495075921211787,
                                                      -2.3182103441722544, -3.297351053358514]
 
 
 def test_python_serial_workflow_function():
-    model_python_serial_function = RunModel(ntasks=1, model_script='python_model.py', model_object_name='sum_rvs', vec=False, verbose=True)
+    model_python_serial_function = RunModel(ntasks=1, model_script='python_model.py', model_object_name='sum_rvs', vec=False, verbose=temp)
     model_python_serial_function.run(samples=x_mcs.samples)
     assert model_python_serial_function.qoi_list == [2.5086338287600496, 0.6605587413536298, 1.7495075921211787,
                                                      -2.3182103441722544, -3.297351053358514]
 
 
 def test_python_parallel_workflow_class():
-    model_python_parallel_class = RunModel(ntasks=3, model_script='python_model.py', model_object_name='SumRVs', verbose=True)
+    model_python_parallel_class = RunModel(ntasks=3, model_script='python_model.py', model_object_name='SumRVs', verbose=temp)
     model_python_parallel_class.run(samples=x_mcs.samples)
     assert model_python_parallel_class.qoi_list == [2.5086338287600496, 0.6605587413536298, 1.7495075921211787,
                                                     -2.3182103441722544, -3.297351053358514]
 
 
 def test_python_parallel_workflow_function():
-    model_python_parallel_function = RunModel(ntasks=3, model_script='python_model.py', model_object_name='sum_rvs', verbose=True)
+    model_python_parallel_function = RunModel(ntasks=3, model_script='python_model.py', model_object_name='sum_rvs', verbose=temp)
     model_python_parallel_function.run(samples=x_mcs.samples)
     assert model_python_parallel_function.qoi_list == [2.5086338287600496, 0.6605587413536298, 1.7495075921211787,
                                                        -2.3182103441722544, -3.297351053358514]
@@ -87,7 +88,7 @@ def test_third_party_serial():
     m = RunModel(ntasks=1, model_script='python_model_sum_scalar.py',
                  input_template='sum_scalar.py', var_names=names, model_object_name="matlab",
                  output_script='process_matlab_output.py', output_object_name='read_output',
-                 resume=False, fmt="{:>10.4f}", verbose=True, delete_files=True)
+                 resume=False, fmt="{:>10.4f}", verbose=temp, delete_files=True)
     m.run(x_mcs.samples)
     assert np.allclose(np.array(m.qoi_list), np.array([2.5086, 0.6605, 1.7495, -2.3183, -3.2974]))
 
@@ -97,6 +98,6 @@ def test_third_party_parallel():
     m = RunModel(ntasks=3, model_script='python_model_sum_scalar.py',
                  input_template='sum_scalar.py', var_names=names, model_object_name="matlab",
                  output_script='process_matlab_output.py', output_object_name='read_output',
-                 resume=False, fmt="{:>10.4f}", verbose=True, delete_files=True)
+                 resume=False, fmt="{:>10.4f}", verbose=temp, delete_files=True)
     m.run(x_mcs.samples)
     assert np.allclose(np.array(m.qoi_list), np.array([2.5086, 0.6605, 1.7495, -2.3183, -3.2974]))
