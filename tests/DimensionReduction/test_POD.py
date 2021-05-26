305737b570e056092648aaa8869f3189d8b91f77
Katiana Kontolati <kkontol1@jhu.edu>
2021-05-26 13:20:34 -0500

    Updates POD tests

--- tests/DimensionReduction/test_POD.py
+++ tests/DimensionReduction/test_POD.py
@@ -30,3 +30,87 @@ def test_SnapshotPOD():
 def test_HOSVD():
     hosvd = HOSVD(input_sol=Data, reconstr_perc=90, verbose=False)
     assert round(hosvd.run(get_error=True)[0][0][1][1], 6) == 0.714928
+
+
+def test_DirectPOD_modes_less_than_zero():
+    pod_dir = DirectPOD(input_sol=Data, modes=-1, verbose=False)
+    a, b = pod_dir.run()
+    assert a == []
+    assert b == []
+
+
+def test_DirectPOD_mode_non_integer():
+    pod_dir = DirectPOD(input_sol=Data, modes=1.5, verbose=False)
+    a, b = pod_dir.run()
+    assert a == []
+    assert b == []
+
+
+def test_DirectPOD_reconstr_perc_less_than_zero():
+    pod_dir = DirectPOD(input_sol=Data, reconstr_perc=-1, verbose=False)
+    a, b = pod_dir.run()
+    assert a == []
+    assert b == []
+
+
+def test_DirectPOD_both_modes_and_reconstr_error():
+    pod_dir = DirectPOD(input_sol=Data, modes=1, reconstr_perc=50, verbose=False)
+    a, b = pod_dir.run()
+    assert a == []
+    assert b == []
+
+
+def test_HOSVD_modes_less_than_zero():
+    hosvd = HOSVD(input_sol=Data, modes=-1, verbose=False)
+    a, b = hosvd.run()
+    assert a == []
+    assert b == []
+
+
+def test_HOSVD_mode_non_integer():
+    hosvd = HOSVD(input_sol=Data, modes=1.5, verbose=False)
+    a, b = hosvd.run()
+    assert a == []
+    assert b == []
+
+
+def test_HOSVD_reconstr_perc_less_than_zero():
+    hosvd = HOSVD(input_sol=Data, reconstr_perc=-1, verbose=False)
+    a, b = hosvd.run()
+    assert a == []
+    assert b == []
+
+
+def test_HOSVD_both_modes_and_reconstr_error():
+    hosvd = HOSVD(input_sol=Data, modes=1, reconstr_perc=50, verbose=False)
+    a, b = hosvd.run()
+    assert a == []
+    assert b == []
+
+
+def test_SnapshotPOD_modes_less_than_zero():
+    snap = SnapshotPOD(input_sol=Data, modes=-1, verbose=False)
+    a, b = snap.run()
+    assert a == []
+    assert b == []
+
+
+def test_SnapshotPOD_mode_non_integer():
+    snap = SnapshotPOD(input_sol=Data, modes=1.5, verbose=False)
+    a, b = snap.run()
+    assert a == []
+    assert b == []
+
+
+def test_SnapshotPOD_reconstr_perc_less_than_zero():
+    snap = SnapshotPOD(input_sol=Data, reconstr_perc=-1, verbose=False)
+    a, b = snap.run()
+    assert a == []
+    assert b == []
+
+
+def test_SnapshotPOD_both_modes_and_reconstr_error():
+    snap = SnapshotPOD(input_sol=Data, modes=1, reconstr_perc=50, verbose=False)
+    a, b = snap.run()
+    assert a == []
+    assert b == []
