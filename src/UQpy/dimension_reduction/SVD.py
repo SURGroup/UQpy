e211626698847c14fc60a1dcce6781bbc3306f2f
dgiovanis <dgiovan1@jhu.edu>
2021-09-28 14:54:03 -0400

    modify SVD

--- src/UQpy/dimension_reduction/SVD.py
+++ src/UQpy/dimension_reduction/SVD.py
@@ -1,35 +1,35 @@
-from typing import Union
 import numpy as np
 
 
 class SVD:
     """
     TODO: Description of SVD
+    TODO: Add test
     """
-    def __init__(self,
-                 matrix: Union[np.ndarray, list],
-                 tolerance: float = None,
-                 rank: int = None,
-                 full_matrices: bool = True,
-                 hermitian: bool = False):
-
-        self.matrix = matrix
-        self.rank = rank
-        self.tolerance = tolerance
-        self.full_matrices = full_matrices
-        self.hermitian = hermitian
-
-    def run(self):
-        ui, si, vi = np.linalg.svd(self.matrix, full_matrices=self.full_matrices, hermitian=self.hermitian)
+    @staticmethod
+    def factorize(matrix, rank=False, tolerance=None, full_matrices=True, hermitian=False):
+        ui, si, vi = np.linalg.svd(matrix, full_matrices=full_matrices, hermitian=hermitian)
         si = np.diag(si)
         vi = vi.T
-        if self.rank is None:
-            self.rank = np.linalg.matrix_rank(self.matrix) if self.tolerance is \
-                                                              None else np.linalg.matrix_rank(self.matrix,
-                                                                                              tol=self.tolerance)
+        if rank:
+            rank = np.linalg.matrix_rank(matrix) if tolerance is None \
+                else np.linalg.matrix_rank(matrix, tol=tolerance)
+            left_eigenvectors = ui[:, :rank]
+            eigenvalues = si[:rank, :rank]
+            right_eigenvectors = vi[:, :rank]
 
-        left_eigenvectors = ui[:, :self.rank]
-        eigenvalues = si[:self.rank, :self.rank]
-        right_eigenvectors = vi[:, :self.rank]
+        elif not rank:
+            left_eigenvectors = ui
+            eigenvalues = si
+            right_eigenvectors = vi
+
+        else:
+            left_eigenvectors = ui[:, :rank]
+            eigenvalues = si[:rank, :rank]
+            right_eigenvectors = vi[:, :rank]
 
         return left_eigenvectors, eigenvalues, right_eigenvectors
+
+    @staticmethod
+    def reconstruct(u, s, v):
+        return np.dot(np.dot(u, s), v.T)
