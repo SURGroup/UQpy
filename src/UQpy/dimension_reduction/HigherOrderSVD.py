cbe4ee652c22cce9c64a89f4f6b1857bfbac0832
dgiovanis <dgiovan1@jhu.edu>
2021-09-29 19:57:23 -0400

    refactor HOSVD

--- src/UQpy/dimension_reduction/HigherOrderSVD.py
+++ src/UQpy/dimension_reduction/HigherOrderSVD.py
@@ -17,46 +17,19 @@ class HigherOrderSVD:
         self.modes = modes
         self.reconstruction_percentage = reconstruction_percentage
 
-    def run(self, get_error: bool = False):
-        """
-        Executes the HOSVD method in the ''HOSVD'' class.
+    def factorize(self, get_error: bool = False):
 
-        **Input:**
+        rows = self.solution_snapshots[0].shape[0]
 
-        * **get_error** (`Boolean`):
-            A boolean declaring whether to return the reconstruction error.
+        a1, a2, a3 = HigherOrderSVD.unfold3d(self.solution_snapshots)
 
-        **Output/Returns:**
-
-        * **reconstructed_solutions** (`ndarray`):
-            Second order tensor containing the reconstructed solution snapshots in their initial spatial and
-            temporal dimensions.
-
-        * **reduced_solutions** (`ndarray`):
-            An array containing the reduced solutions snapshots. The array's dimensions depends on the dimensions
-            of input second order tensor and not on the input number of modes or reconstruction percentage.
-
-        """
-
-        if type(self.solution_snapshots) == list:
-            rows = self.solution_snapshots[0].shape[0]
-            columns = self.solution_snapshots[0].shape[1]
-            snapshot_number = len(self.solution_snapshots)
-        else:
-            rows = self.solution_snapshots.shape[0]
-            columns = self.solution_snapshots.shape[1]
-            snapshot_number = self.solution_snapshots.shape[2]
-
-        a1, a2, a3 = HigherOrderSVD.unfold(self.solution_snapshots)
-
-        u1, sig_1, v1 = SVD(a1, full_matrices=True).run()
-        u2, sig_2, v2 = SVD(a2, full_matrices=True).run()
-        u3, sig_3, v3 = SVD(a3, full_matrices=True).run()
+        u1, sig_1, v1 = SVD.factorize(a1)
+        u2, sig_2, v2 = SVD.factorize(a2)
+        u3, sig_3, v3 = SVD.factorize(a3)
 
         sig_3_ = np.diag(sig_3)
         hold = np.dot(np.linalg.inv(u3), a3)
         kronecker_product = np.kron(u1, u2)
-
         s3 = np.array(np.dot(hold, np.linalg.inv(kronecker_product.T)))
 
         if self.modes <= 0:
@@ -98,42 +71,19 @@ class HigherOrderSVD:
             u3hat = np.dot(u3[:, :self.modes], sig_3_[:self.modes, :self.modes])
             s3hat = np.dot(np.linalg.inv(sig_3_[:self.modes, :self.modes]), s3[:self.modes, :])
 
-            b = np.kron(u1, u2)
-            c = np.dot(s3hat, b.T)
-            d = np.dot(u3hat[:, :], c)
-
-            reconstructed_solutions = np.zeros((rows, columns, snapshot_number))
-            for i in range(snapshot_number):
-                reconstructed_solutions[0:rows, 0:columns, i] = d[i, :].reshape((rows, columns))
-
-            self.logger.info("UQpy: Successful execution of HOSVD!")
-
-            if self.modes == 10**10:
-                self.logger.info('Dataset reconstruction: {0:.3%}'.format(self.reconstruction_percentage / 100))
+            if self.modes == 10 ** 10:
+                self.logger.info(
+                    'Dataset reconstruction: {0:.3%}'.format(self.reconstruction_percentage / 100))
 
             else:
                 if get_error:
-                    error_rec = np.sqrt(((sig_3[self.modes:]) ** 2).sum()) / np.sqrt((sig_3 ** 2).sum())
+                    error_rec = np.sqrt(((s3hat[self.modes:]) ** 2).sum()) / np.sqrt((sig_3 ** 2).sum())
                     self.logger.warning("Reduced-order reconstruction error: {0:.3%}".format(error_rec))
 
-            return reconstructed_solutions, reduced_solutions
+            return u1, u2, u3, u3hat, s3hat
 
     @staticmethod
-    def unfold(second_order_tensor):
-        """
-        Method for unfolding second order tensors.
-
-        **Input:**
-
-        * **data** (`ndarray`) or (`list`):
-            Input second order tensor to be unfolded.
-
-        **Output/Returns:**
-
-        * **M0, M1, M2** (`ndarrays`):
-            Returns the 2-dimensional unfolded matrices.
-        """
-
+    def unfold3d(second_order_tensor):
         if type(second_order_tensor) == list:
             rows = second_order_tensor[0].shape[0]
             columns = second_order_tensor[0].shape[1]
@@ -160,3 +110,23 @@ class HigherOrderSVD:
                                            second_order_tensor.shape[0] * second_order_tensor.shape[1])
 
         return matrix1, matrix2, matrix3
+
+    @staticmethod
+    def reconstruct(u1, u2,  u3hat, s3hat):
+
+        b = np.kron(u1, u2)
+        c = np.dot(s3hat, b.T)
+        d = np.dot(u3hat[:, :], c)
+
+        rows = u1.shape[0]
+        columns = u2.shape[0]
+        snapshot_number = u3hat.shape[0]
+
+        reconstructed_solutions = np.zeros((rows, columns, snapshot_number))
+        for i in range(snapshot_number):
+            reconstructed_solutions[0:rows, 0:columns, i] = d[i, :].reshape((rows, columns))
+
+        logging.getLogger(__name__).info("UQpy: Successful execution of HOSVD!")
+
+        return reconstructed_solutions
+
