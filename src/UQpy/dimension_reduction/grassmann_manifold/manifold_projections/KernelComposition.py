ceff2bda1a0d7fac8776711b1553db26981b0a0f
Dimitris Tsapetis <dimtsap@hotmail.com>
2021-11-04 19:00:12 -0400

    Changes KernelComposition documentation

--- src/UQpy/dimension_reduction/grassmann_manifold/manifold_projections/KernelComposition.py
+++ src/UQpy/dimension_reduction/grassmann_manifold/manifold_projections/KernelComposition.py
@@ -12,20 +12,12 @@ class KernelComposition(Enum):
     """
     This is an enumeration which is a set of symbolic names (members) bound to unique, constant values. It is used when
     the SVD is used to project points onto the Grassmann manifold.
-
-    Options:
-
-        1. KernelComposition.LEFT --> The matrix of left eigenvectors will be used for creating a Grassmann kernel.
-
-        2. KernelComposition.RIGHT --> The matrix of right eigenvectors will be used for creating a Grassmann kernel.
-
-        3. KernelComposition.PRODUCT --> The kernel will result from the product of the two above kernels.
-
-        4. KernelComposition.SUM --> The kernel will result from the summation of the two above kernels.
-    """
-
+   """
+    #: The matrix of left eigenvectors will be used for creating a Grassmann kernel.
     LEFT = (1,)
-    """The matrix of left eigenvectors will be used for creating a Grassmann kernel."""
+    #: The matrix of right eigenvectors will be used for creating a Grassmann kernel.
     RIGHT = (2,)
-    PRODUCT = (3,)
+    #: The kernel will result from the product of the two above kernels.
+    PRODUCT = (3,)  # doc:
+    #: The kernel will result from the summation of the two above kernels.
     SUM = 4
