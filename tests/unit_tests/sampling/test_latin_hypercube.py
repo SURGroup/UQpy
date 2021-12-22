fb8e9082d0b77ecd0ae57896c28d2324d8ad02ca
Dimitris Tsapetis <dimtsap@hotmail.com>
2021-12-22 15:33:56 +0200

    Fixes latin hypercube tests (#131)

--- tests/unit_tests/sampling/test_latin_hypercube.py
+++ tests/unit_tests/sampling/test_latin_hypercube.py
@@ -13,30 +13,30 @@ distribution1 = Uniform(1, 2)
 distribution2 = Uniform(3, 4)
 
 def test_lhs_random_criterion():
-    random_criterion = Random(random_state=1)
+    random_criterion = Random()
     latin_hypercube_sampling = \
-        LatinHypercubeSampling(distributions=distribution, samples_number=2,
-                               criterion=random_criterion)
+        LatinHypercubeSampling(distributions=distribution, nsamples=2,
+                               criterion=random_criterion, random_state=1)
     actual_samples = latin_hypercube_sampling.samples.flatten()
     expected_samples = np.array([[1.208511], [1.86016225]]).flatten()
     np.testing.assert_allclose(expected_samples, actual_samples, rtol=1e-6)
 
 
 def test_lhs_centered_criterion():
-    centered_criterion = Centered(random_state=1)
+    centered_criterion = Centered()
     latin_hypercube_sampling = \
-        LatinHypercubeSampling(distributions=distribution, samples_number=2,
-                               criterion=centered_criterion)
+        LatinHypercubeSampling(distributions=distribution, nsamples=2,
+                               criterion=centered_criterion, random_state=1)
     actual_samples = latin_hypercube_sampling.samples.flatten()
     expected_samples = np.array([[1.25], [1.75]]).flatten()
     np.testing.assert_allclose(expected_samples, actual_samples, rtol=1e-6)
 
 
 def test_lhs_maximin_criterion():
-    maximin_criterion = MaxiMin(random_state=1)
+    maximin_criterion = MaxiMin()
     latin_hypercube_sampling = \
-        LatinHypercubeSampling(distributions=distribution, samples_number=2,
-                               criterion=maximin_criterion)
+        LatinHypercubeSampling(distributions=distribution, nsamples=2,
+                               criterion=maximin_criterion, random_state=1)
     actual_samples = latin_hypercube_sampling.samples.flatten()
     expected_samples = np.array([[1.208511], [1.86016225]]).flatten()
     np.testing.assert_allclose(expected_samples, actual_samples, rtol=1e-6)
