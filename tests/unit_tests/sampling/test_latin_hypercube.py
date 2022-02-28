0cd257375e4c9c0a456f656c5ba71a997334011a
Dimitris Tsapetis <dimtsap@hotmail.com>
2022-02-28 17:59:06 -0500

    Increases LHS coverage

--- tests/unit_tests/sampling/test_latin_hypercube.py
+++ tests/unit_tests/sampling/test_latin_hypercube.py
@@ -45,3 +45,42 @@ def test_lhs_maximin_criterion_joint_independent():
     actual_samples = latin_hypercube_sampling.samples.flatten()
     expected_samples = np.array([1.86016225, 1.00011437, 1.208511,   2.30233257]).flatten()
     np.testing.assert_allclose(expected_samples, actual_samples, rtol=1e-6)
+
+
+from scipy.spatial.distance import pdist
+
+dist1 = Uniform(loc=0., scale=1.)
+dist2 = Uniform(loc=0., scale=1.)
+joint_dist = JointIndependent(marginals=[dist1, dist2])
+x1b = LatinHypercubeSampling(distributions=joint_dist, criterion=MinCorrelation(), nsamples=5, random_state=123)
+
+x1d = LatinHypercubeSampling(distributions=dist1, criterion=Centered(), random_state=123, nsamples=5)
+
+x1e = LatinHypercubeSampling(distributions=dist1, criterion=Centered(), nsamples=5)
+x1g = LatinHypercubeSampling(distributions=[dist1, dist2], nsamples=5)
+
+
+def d_func(x): return pdist(x, metric='euclidean')
+
+
+x1h = LatinHypercubeSampling(distributions=[dist1, dist2], criterion=MaxiMin(), nsamples=5, random_state=789)
+
+
+def test_samples2():
+    """ Check the samples attribute, when dist_object is a jointInd class object and criterion is 'correlate'."""
+    expected_samples = np.array([[0.94, 0.54], [0.26, 0.08], [0.45, 0.88], [0.14, 0.7 ], [0.71, 0.4 ]])
+    assert (x1b.samples.round(2) == expected_samples).all()
+
+
+def test_samples4():
+    """ Check the samples attribute, when dist_object is a list of distribution class object and criterion is
+    'centered'."""
+    expected_samples = np.array([0.1, 0.9, 0.5, 0.3, 0.7])
+    assert (x1d.samples.round(2) == expected_samples).all()
+
+
+def test_samples5():
+    """ Check the samples attribute, when dist_object is a list of distribution class object, criterion is
+    'maximin' and metric is callable."""
+    expected_samples = np.array([[0.56, 0.2 ], [0.25, 0.62], [1.  , 0.4 ], [0.72, 0.91], [0.06, 0.15]])
+    assert (x1h.samples.round(2) == expected_samples).all()
