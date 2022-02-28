560f36e648f1e6731f0ac472bf42343607545909
Dimitris Tsapetis <dimtsap@hotmail.com>
2022-02-28 18:17:32 -0500

    Increases AKMCS coverage

--- tests/unit_tests/sampling/test_adaptive_kriging.py
+++ tests/unit_tests/sampling/test_adaptive_kriging.py
@@ -1,5 +1,7 @@
-from UQpy.optimization.MinimizeOptimizer import MinimizeOptimizer
-from matplotlib import pyplot as plt
+import pytest
+
+from UQpy import Multinomial
+from UQpy.utilities.MinimizeOptimizer import MinimizeOptimizer
 
 from UQpy.surrogates.kriging.Kriging import Kriging
 from UQpy.sampling import MonteCarloSampling, AdaptiveKriging
@@ -133,170 +135,46 @@ def test_akmcs_expected_improvement_global_fit():
 
     shutil.rmtree(rmodel.model_dir)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def test_akmcs_samples_error():
+    from UQpy.surrogates.kriging.regression_models.LinearRegression import Linear
+    from UQpy.surrogates.kriging.correlation_models.ExponentialCorrelation import Exponential
+
+    marginals = [Normal(loc=0., scale=4.), Normal(loc=0., scale=4.)]
+    x = MonteCarloSampling(distributions=marginals, nsamples=20, random_state=0)
+    rmodel = RunModel(model_script='series.py', vec=False)
+    regression_model = Linear()
+    correlation_model = Exponential()
+    K = Kriging(regression_model=regression_model, correlation_model=correlation_model,
+                optimizer=MinimizeOptimizer('l-bfgs-b'),
+                optimizations_number=10, correlation_model_parameters=[1, 1], random_state=1)
+    # OPTIONS: 'U', 'EFF', 'Weighted-U'
+    learning_function = WeightedUFunction(weighted_u_stop=2)
+    with pytest.raises(NotImplementedError):
+        a = AdaptiveKriging(distributions=[Normal(loc=0., scale=4.)]*3, runmodel_object=rmodel, surrogate=K,
+                            learning_nsamples=10**3, n_add=1, learning_function=learning_function,
+                            random_state=2, samples=x.samples)
+
+    shutil.rmtree(rmodel.model_dir)
+
+def test_akmcs_u_run_from_init():
+    from UQpy.surrogates.kriging.regression_models.LinearRegression import Linear
+    from UQpy.surrogates.kriging.correlation_models.ExponentialCorrelation import Exponential
+
+    marginals = [Normal(loc=0., scale=4.), Normal(loc=0., scale=4.)]
+    x = MonteCarloSampling(distributions=marginals, nsamples=20, random_state=1)
+    rmodel = RunModel(model_script='series.py', vec=False)
+    regression_model = Linear()
+    correlation_model = Exponential()
+    K = Kriging(regression_model=regression_model, correlation_model=correlation_model,
+                optimizer=MinimizeOptimizer('l-bfgs-b'),
+                optimizations_number=10, correlation_model_parameters=[1, 1], random_state=0)
+    # OPTIONS: 'U', 'EFF', 'Weighted-U'
+    learning_function = UFunction(u_stop=2)
+    a = AdaptiveKriging(distributions=marginals, runmodel_object=rmodel, surrogate=K,
+                        learning_nsamples=10**3, n_add=1, learning_function=learning_function,
+                        random_state=2, nsamples=25, samples=x.samples)
+
+    assert a.samples[23, 0] == -4.141979058326188
+    assert a.samples[20, 1] == -1.6476534435429009
+
+    shutil.rmtree(rmodel.model_dir)
