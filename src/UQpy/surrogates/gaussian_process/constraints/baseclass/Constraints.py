7393f8717ba05be86cb7e10fde4bc17023ad97d3
Mohit Singh Chauhan <mchauha1@jhu.edu>
2022-04-03 23:52:41 -0400

    GaussianProcessRegressor: Small changes in Constraints.

--- src/UQpy/surrogates/gaussian_process/constraints/baseclass/Constraints.py
+++ src/UQpy/surrogates/gaussian_process/constraints/baseclass/Constraints.py
@@ -8,7 +8,7 @@ class ConstraintsGPR(ABC):
     """
 
     @abstractmethod
-    def constraints(self, x_train, y_train, predict_function):
+    def define_arguments(self, x_train, y_train, predict_function):
         """
         Abstract method that needs to be implemented by the user when creating a new Correlation function.
         """
