92a724c1efd93d2af33233fd5491afc3b773ab21
Prateek Bhustali <p.bhustali@tu-braunschweig.de>
2022-05-08 23:48:32 +0200

    Added Chatterjee sensitivity index

--- src/UQpy/sensitivity/__init__.py
+++ src/UQpy/sensitivity/__init__.py
@@ -2,8 +2,10 @@ from UQpy.sensitivity.MorrisSensitivity import MorrisSensitivity
 from UQpy.sensitivity.PceSensitivity import PceSensitivity
 from UQpy.sensitivity.sobol import Sobol
 from UQpy.sensitivity.cramer_von_mises import CramervonMises
+from UQpy.sensitivity.chatterjee import Chatterjee
 
 from . import MorrisSensitivity
 from . import PceSensitivity
 from . import Sobol
 from . import CramervonMises
+from . import Chatterjee
