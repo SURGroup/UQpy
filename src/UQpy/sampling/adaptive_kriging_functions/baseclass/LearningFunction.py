5a9c338aea3695e0488288981b9fe069c1dec1f3
Dimitris Tsapetis <dimtsap@hotmail.com>
2022-11-10 10:00:06 -0500

    Fixes Nataf Stability issue by adding an n_gauss_points optional parameter to distortion_z2x function

--- src/UQpy/sampling/adaptive_kriging_functions/baseclass/LearningFunction.py
+++ src/UQpy/sampling/adaptive_kriging_functions/baseclass/LearningFunction.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class LearningFunction(ABC):
     def __init(self, ordered_parameters=None, **kwargs):
         self.parameters = kwargs
-        self.ordered_parameters = (ordered_parameters if not None else tuple(kwargs.keys()))
+        self.ordered_parameters = (ordered_parameters if ordered_parameters is not None else tuple(kwargs.keys()))
         if len(self.ordered_parameters) != len(self.parameters):
             raise ValueError("Inconsistent dimensions between order_params tuple and params dictionary.")
 
