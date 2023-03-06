5564ab8048dc632ab005628b19b17fcc8d30dbe4
Dimitris Tsapetis <dimtsap@hotmail.com>
2023-03-06 10:18:38 -0500

    Refactors Gaussian process kernels to abstract from hyperparameters

--- src/UQpy/surrogates/gaussian_process/kernels/RBF.py
+++ src/UQpy/surrogates/gaussian_process/kernels/RBF.py
@@ -1,15 +1,18 @@
+from typing import Union
+
 from UQpy.surrogates.gaussian_process.kernels.baseclass.Kernel import *
 
 
 class RBF(Kernel):
-    def c(self, x, s, params):
+    def __init__(self, kernel_parameter: Union[int, float] = 1.0):
+        super().__init__(kernel_parameter)
+
+    def calculate_kernel_matrix(self, x, s):
         """
         This method compute the RBF kernel on sample points 'x' and 's'.
 
-        :params x: An array containing input samples.
-        :params s: An array containing input samples.
-        :params params: A list/array of hyperparameters containing length scales and the process variance.
+        :params x: An array containing training points.
+        :params s: An array containing input points.
         """
-        stack = Kernel.check_samples_and_return_stack(x/params[:-1], s/params[:-1])
-        k = params[-1] ** 2 * np.exp(np.sum(-0.5 * (stack ** 2), axis=2))
-        return k
+        stack = Kernel.check_samples_and_return_stack(x / self.kernel_parameter, s / self.kernel_parameter)
+        return np.exp(np.sum(-0.5 * (stack ** 2), axis=2))
