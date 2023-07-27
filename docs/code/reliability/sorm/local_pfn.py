174d578c17c4811415e47b4d3594bc6addf574ab
Dimitris Tsapetis <dimtsap@hotmail.com>
2023-07-27 17:13:43 +0300

    Minor changes to fix reliability examples

--- docs/code/reliability/sorm/local_pfn.py
+++ docs/code/reliability/sorm/local_pfn.py
@@ -6,14 +6,15 @@ Auxiliary file
 """
 import numpy as np
 
+
 def example1(samples=None):
     g = np.zeros(samples.shape[0])
-    for i in range(samples.shape[0]):         
+    for i in range(samples.shape[0]):
         R = samples[i, 0]
         S = samples[i, 1]
         g[i] = R - S
     return g
-    
+
 
 def example2(samples=None):
     import numpy as np
@@ -21,18 +22,15 @@ def example2(samples=None):
     beta = 3.0902
     g = np.zeros(samples.shape[0])
     for i in range(samples.shape[0]):
-        g[i] = -1/np.sqrt(d) * (samples[i, 0] + samples[i, 1]) + beta
+        g[i] = -1 / np.sqrt(d) * (samples[i, 0] + samples[i, 1]) + beta
     return g
 
 
 def example3(samples=None):
     g = np.zeros(samples.shape[0])
     for i in range(samples.shape[0]):
-        g[i] = 6.2*samples[i, 0] - samples[i, 1]*samples[i, 2]**2
+        g[i] = 6.2 * samples[i, 0] - samples[i, 1] * samples[i, 2] ** 2
     return g
-    
-def example4(samples=None):
-    g = np.zeros(samples.shape[0])
-    for i in range(samples.shape[0]):
-        g[i] = samples[i, 0]*samples[i, 1] - 80
-    return g
+
+
+
