f15952bd6181fe4af2cf04c1be73e7ce51aa607f
connor-krill <ckrill1@jh.edu>
2024-03-20 14:32:10 -0400

    improved documentation and beartypes for Dropout Layer

--- src/UQpy/scientific_machine_learning/layers/Dropout.py
+++ src/UQpy/scientific_machine_learning/layers/Dropout.py
@@ -1,18 +1,44 @@
-from UQpy.scientific_machine_learning.baseclass.Layer import Layer
-# Modified by George and Ponkrshnan
+import torch
 import torch.nn as nn
+import beartype
+from beartype.vale import Is
+from typing import Annotated
+from UQpy.scientific_machine_learning.baseclass.Layer import Layer
 
+
+@beartype
 class Dropout(Layer):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(
+        self,
+        drop_rate: Annotated[float, Is[lambda p: 0 <= p <= 1]] = 0.5,
+        drop_type: int = 0,
+        **kwargs
+    ):
+        """
 
-    def forward(self, x, p = 0.5):
-        self.drop_rate = p
-        # this should be determined based on the dimensions of x N x C x H x W
-        # N x features
-        if len(x.shape) > 2:
-            self.dropout = nn.Dropout2d(self.drop_rate)
-        else:
-            self.dropout = nn.Dropout(self.drop_rate)
-        x = self.dropout(x)
+        :param drop_rate:
+        :param drop_type:
+        :param kwargs:
+        """
+        super().__init__(**kwargs)
+        self.drop_rate = drop_rate
+        self.drop_type = drop_type
+        if self.drop_type == 0:
+            self.dropout_function = nn.Dropout(self.drop_rate)
+        elif self.drop_type == 1:
+            self.dropout_function = nn.Dropout(self.drop_rate)
+        elif self.drop_type == 2:
+            self.dropout_function = nn.Dropout(self.drop_rate)
+        elif self.drop_type == 3:
+            self.dropout_function = nn.Dropout3d(self.drop_rate)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward computational call
+
+        Note: Function behavior is determind by the dimensions of ``x`` as N x C x H x W
+
+        :param x:
+        :return:
+        """
+        return self.dropout_function(x)
