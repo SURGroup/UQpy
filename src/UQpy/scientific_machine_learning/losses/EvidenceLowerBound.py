c42e691d0f7c5157353f644f394c6ee3dd6f56d7
ponkrshnan <thiagra@mtu.edu>
2024-03-20 15:35:47 -0400

    Added beta in forward

--- src/UQpy/scientific_machine_learning/losses/EvidenceLowerBound.py
+++ src/UQpy/scientific_machine_learning/losses/EvidenceLowerBound.py
@@ -6,24 +6,23 @@ from UQpy.scientific_machine_learning.baseclass.Loss import Loss
 
 @beartype
 class EvidenceLowerBound(Loss):
-    def __init__(self, train_size: int, beta: float, **kwargs):  
+    def __init__(self, train_size: int, **kwargs):  
         """
 
         :param train_size: Number of training samples
-        :param beta: Weighting factor for the KL term
         :param kwargs:
         """
         super().__init__(**kwargs)
         self.train_size = train_size
-        self.beta = beta
 
 
-    def forward(self, x, target, kl) -> torch.Tensor:
+    def forward(self, x, target, kl, beta) -> torch.Tensor:
         """
         
         :param x:
         :param target:
         :param kl:
+        :param beta: Weighting factor for the KL term
         :return:
         """
-        return F.nll_loss(x, target, reduction="mean") * self.train_size + self.beta * kl
+        return F.nll_loss(x, target, reduction="mean") * self.train_size + beta * kl
