46d45a7c42238bdbd70c7837f978d51c3ad77475
connor-krill <ckrill1@jh.edu>
2024-04-17 12:20:36 -0400

    clarified test names, added BNN tests

--- tests/unit_tests/scientific_machine_learning/test_bayesian_layer.py
+++ tests/unit_tests/scientific_machine_learning/test_bayesian_layer.py
@@ -17,35 +17,38 @@ def test_output_shape(in_features, out_features):
     assert y.shape == torch.Size([out_features])
 
 
-def test_sample_false():
+def test_sample_false_train_false():
     in_features = 5
     out_features = 10
     layer = BayesianLayer(in_features, out_features)
     layer.train(False)
+    layer.sample(False)
     x = torch.ones((in_features,))
 
-    y1 = layer(x, sample=False)
-    y2 = layer(x, sample=False)
+    y1 = layer(x)
+    y2 = layer(x)
     assert torch.allclose(y1, y2)
 
 
-def test_sample_true():
+def test_sample_true_train_false():
     in_features = 5
     out_features = 10
     layer = BayesianLayer(in_features, out_features)
     layer.train(False)
+    layer.sample(True)
     x = torch.ones((in_features,))
 
-    y1 = layer(x, sample=True)
-    y2 = layer(x, sample=True)
+    y1 = layer(x)
+    y2 = layer(x)
     assert not torch.allclose(y1, y2)
 
 
-def test_train_true():
+def test_sample_false_train_true():
     in_features = 5
     out_features = 10
-    layer = BayesianLayer(in_features, out_features, sample=False)
+    layer = BayesianLayer(in_features, out_features)
     layer.train(True)
+    layer.sample(False)
     x = torch.ones((in_features,))
 
     y1 = layer(x)
@@ -56,5 +59,5 @@ def test_train_true():
 def test_extra_repr():
     layer = BayesianLayer(5, 7)
     assert (
-        layer.__str__() == "BayesianLayer(in_features=5, out_features=7, sample=True)"
+        layer.__str__() == "BayesianLayer(in_features=5, out_features=7, sampling=True)"
     )
--- tests/unit_tests/scientific_machine_learning/test_bayesian_neural_network.py
+++ tests/unit_tests/scientific_machine_learning/test_bayesian_neural_network.py
@@ -1,8 +1,10 @@
 import torch
-import torch.nn
+import torch.nn as nn
 from hypothesis import given
 from hypothesis.strategies import integers
-from UQpy.scientific_machine_learning.neural_networks.BayesianNeuralNetwork import BayesianNeuralNetwork
+from UQpy.scientific_machine_learning.neural_networks.BayesianNeuralNetwork import (
+    BayesianNeuralNetwork,
+)
 from UQpy.scientific_machine_learning.layers.BayesianLayer import BayesianLayer
 
 
@@ -12,3 +14,44 @@ def test_output_shape(in_features, out_features):
     x = torch.ones((in_features,))
     assert model(x).shape == torch.Size([out_features])
 
+
+def test_deterministic_output():
+    in_features = 100
+    width = 5
+    out_features = 200
+    network = nn.Sequential(
+        BayesianLayer(in_features, width),
+        nn.ReLU(),
+        nn.Linear(width, width),
+        nn.ReLU(),
+        BayesianLayer(width, out_features),
+    )
+    model = BayesianNeuralNetwork(network)
+    model.train(False)
+    model.sample(False)
+
+    x = torch.ones((in_features,))
+    y1 = model(x)
+    y2 = model(x)
+    assert torch.allclose(y1, y2)
+
+
+def test_probabilistic_output():
+    in_features = 5
+    width = 7
+    out_features = 3
+    network = nn.Sequential(
+        BayesianLayer(in_features, width),
+        nn.ReLU(),
+        nn.Linear(width, width),
+        nn.ReLU(),
+        BayesianLayer(width, out_features),
+    )
+    model = BayesianNeuralNetwork(network)
+    model.train(False)
+    model.sample(True)
+
+    x = torch.ones((in_features,))
+    y1 = model(x)
+    y2 = model(x)
+    assert not torch.allclose(y1, y2)
