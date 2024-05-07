6469fe8a58c5be50ec26a2001421a3b215111e0b
connor-krill <ckrill1@jh.edu>
2024-05-07 14:12:38 -0400

    deleted unused function

--- src/UQpy/stochastic_process/supportive/__init__.py
+++ src/UQpy/stochastic_process/supportive/__init__.py
@@ -5,6 +5,3 @@ from UQpy.stochastic_process.supportive.inverse_wiener_khinchin_transform import
 from UQpy.stochastic_process.supportive.wiener_khinchin_transform import (
     wiener_khinchin_transform,
 )
-from UQpy.stochastic_process.supportive.scaling_correlation_function import (
-    scaling_correlation_function,
-)
