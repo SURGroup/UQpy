d6e77d7f74900b9fd54336dc46ff715d95380ffc
ponkrshnan <ponkrshnan@jhu.edu>
2024-05-14 16:09:41 -0400

    Added VI sinusoidal data example

---
+++ docs/code/scientific_machine_learning/bbb_trainer/NeuralNetwork_VI.py
@@ -0,0 +1,118 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+
+# UQpy imports
+from UQpy.scientific_machine_learning.layers import BayesianLayer
+from UQpy.scientific_machine_learning.neural_networks import FeedForwardNeuralNetwork
+from UQpy.scientific_machine_learning.trainers import BBBTrainer
+
+import logging
+
+logger = logging.getLogger("UQpy")
+logger.setLevel(logging.INFO)
+
+width = 20
+network = nn.Sequential(
+    BayesianLayer(1, width),
+    nn.ReLU(),
+    BayesianLayer(width, width),
+    nn.ReLU(),
+    BayesianLayer(width, width),
+    nn.ReLU(),
+    BayesianLayer(width, width),
+    nn.ReLU(),
+    BayesianLayer(width, 1),
+)
+model = FeedForwardNeuralNetwork(network)
+
+
+class SinusoidalDataset(Dataset):
+    def __init__(self, n_samples=20, noise_std=0.05):
+        self.n_samples = n_samples
+        self.noise_std = noise_std
+        self.x = torch.linspace(-1, 1, n_samples).reshape(-1, 1)
+        self.y = torch.tensor(0.4 * np.sin(4 * self.x) + 0.5 * np.cos(12 * self.x) +
+                              np.random.normal(0, self.noise_std, self.x.shape), dtype=torch.float)
+
+    def __len__(self):
+        return self.n_samples
+
+    def __getitem__(self, item):
+        return self.x[item], self.y[item]
+
+
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+train_data = DataLoader(SinusoidalDataset(), batch_size=20, shuffle=True)
+trainer = BBBTrainer(model, optimizer)
+print("Starting Training...", end="")
+trainer.run(train_data=train_data, epochs=5000, beta=1e-6, num_samples=10)
+print("done")
+
+# Plotting Results
+x = SinusoidalDataset().x
+y = SinusoidalDataset().y
+model.train(False)
+model.sample(False)
+final_prediction = model(x)
+fig, ax = plt.subplots()
+ax.plot(
+    x.detach().numpy(),
+    y.detach().numpy(), '*',
+    label="Data",
+    color="tab:blue",
+)
+ax.plot(
+    x.detach().numpy(),
+    final_prediction.detach().numpy(),
+    label="Final Prediction",
+    color="tab:orange",
+)
+ax.plot(
+    x.detach().numpy(),
+    (0.4 * np.sin(4 * x) + 0.5 * np.cos(12 * x)).detach().numpy(),
+    label="Exact",
+    color="black",
+    linestyle="dashed",
+)
+
+ax.set_title("Predictions")
+ax.set(xlabel="x", ylabel="f(x)")
+ax.legend()
+
+train_loss = trainer.history["train_loss"].detach().numpy()
+fig, ax = plt.subplots()
+ax.plot(train_loss)
+ax.set_title("Bayes By Backpropagation Training Loss")
+ax.set(xlabel="Epoch", ylabel="Loss")
+
+plt.show()
+
+model.sample(False)
+print("BNN is deterministic:", model.is_deterministic())
+mean = model(x)
+model.sample(True)
+print("BNN is deterministic:", model.is_deterministic())
+n = 10_000
+samples = torch.zeros(len(x), n)
+for i in range(n):
+    samples[:, i] = model(x).squeeze()
+variance = torch.var(samples, dim=1)
+standard_deviation = torch.sqrt(variance)
+
+x_plot = x.squeeze().detach().numpy()
+mu = mean.squeeze().detach().numpy()
+sigma = standard_deviation.squeeze().detach().numpy()
+fig, ax = plt.subplots()
+ax.plot(x_plot, mu, label="$\mu$")
+ax.plot(x_plot, y.detach().numpy(), label="Exact", color="black", linestyle="dashed")
+ax.fill_between(
+    x_plot, mu - (3 * sigma), mu + (3 * sigma), label="$\mu \pm 3\sigma$,", alpha=0.3
+)
+ax.set_title("Bayesian Neural Network $\mu \pm 3\sigma$")
+ax.set(xlabel="x", ylabel="f(x)")
+ax.legend()
+
+plt.show()
