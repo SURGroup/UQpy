25fc0fee5da3971b3d47aa3b5fce19e2ebf0a479
ponkrshnan <ponkrshnan@jhu.edu>
2024-05-15 14:18:15 -0400

    Added validation data

--- docs/code/scientific_machine_learning/bbb_trainer/NeuralNetwork_VI.py
+++ docs/code/scientific_machine_learning/bbb_trainer/NeuralNetwork_VI.py
@@ -54,9 +54,12 @@ print("done")
 # Plotting Results
 x = SinusoidalDataset().x
 y = SinusoidalDataset().y
+x_val = torch.linspace(-1, 1, 1000).view(-1,1)
+y_val = (0.4 * torch.sin(4 * x_val) + 0.5 * torch.cos(12 * x_val))
 model.train(False)
 model.sample(False)
-final_prediction = model(x)
+# final_prediction = model(x)
+pred_val = model(x_val)
 fig, ax = plt.subplots()
 ax.plot(
     x.detach().numpy(),
@@ -65,14 +68,13 @@ ax.plot(
     color="tab:blue",
 )
 ax.plot(
-    x.detach().numpy(),
-    final_prediction.detach().numpy(),
+    x_val.detach().numpy(),
+    pred_val.detach().numpy(),
     label="Final Prediction",
     color="tab:orange",
 )
 ax.plot(
-    x.detach().numpy(),
-    (0.4 * np.sin(4 * x) + 0.5 * np.cos(12 * x)).detach().numpy(),
+    x_val.detach(), y_val.detach(),
     label="Exact",
     color="black",
     linestyle="dashed",
@@ -92,13 +94,13 @@ plt.show()
 
 model.sample(False)
 print("BNN is deterministic:", model.is_deterministic())
-mean = model(x)
+mean = model(x_val)
 model.sample(True)
 print("BNN is deterministic:", model.is_deterministic())
-n = 10_000
-samples = torch.zeros(len(x), n)
+n = 1000
+samples = torch.zeros(len(x_val), n)
 for i in range(n):
-    samples[:, i] = model(x).squeeze()
+    samples[:, i] = model(x_val).squeeze()
 variance = torch.var(samples, dim=1)
 standard_deviation = torch.sqrt(variance)
 
@@ -106,10 +108,10 @@ x_plot = x.squeeze().detach().numpy()
 mu = mean.squeeze().detach().numpy()
 sigma = standard_deviation.squeeze().detach().numpy()
 fig, ax = plt.subplots()
-ax.plot(x_plot, mu, label="$\mu$")
-ax.plot(x_plot, y.detach().numpy(), label="Exact", color="black", linestyle="dashed")
+ax.plot(x_val, mu, label="$\mu$")
+ax.plot(x_val.detach(), y_val.detach(), label="Exact", color="black", linestyle="dashed")
 ax.fill_between(
-    x_plot, mu - (3 * sigma), mu + (3 * sigma), label="$\mu \pm 3\sigma$,", alpha=0.3
+    x_val.view(-1).detach(), mu - (3 * sigma), mu + (3 * sigma), label="$\mu \pm 3\sigma$,", alpha=0.3
 )
 ax.set_title("Bayesian Neural Network $\mu \pm 3\sigma$")
 ax.set(xlabel="x", ylabel="f(x)")
