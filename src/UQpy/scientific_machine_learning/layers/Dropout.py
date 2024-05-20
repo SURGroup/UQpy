56b864096652257136414e3a027661df8e092b14
ponkrshnan <ponkrshnan@jhu.edu>
2024-05-20 10:19:48 -0400

    Added drop attribute to dropout layers

--- src/UQpy/scientific_machine_learning/layers/Dropout.py
+++ src/UQpy/scientific_machine_learning/layers/Dropout.py
@@ -13,17 +13,20 @@ class Dropout(Layer):
         self,
         drop_rate: Annotated[float, Is[lambda p: 0 <= p <= 1]] = 0.5,
         drop_type: int = 0,
+        drop: bool = True,
         **kwargs
     ):
         """Initialize a dropout layer to randomly zero components of a tensor
 
         :param drop_rate: Probability of a dropout occurring
         :param drop_type: If ``0`` drop elements, ``1`` drops vectors, ``2`` drops channels, ``3`` drops 3D
+        :param drop: IF True apply drop out
         :param kwargs:
         """
         super().__init__(**kwargs)
         self.drop_rate = drop_rate
         self.drop_type = drop_type
+        self.drop = drop
         if self.drop_type == 0:
             self.dropout_function = nn.Dropout(self.drop_rate)
         elif self.drop_type == 1:
@@ -41,7 +44,14 @@ class Dropout(Layer):
         :param x:
         :return:
         """
-        return self.dropout_function(x)
+        return self.dropout_function(x) if self.drop else x
+
+    def drop(self, mode: bool = True):
+        """Set sampling mode.
+
+        :param mode: If ``True``, layer parameters are dropped.
+        """
+        self.drop = mode
 
     def extra_repr(self) -> str:
         return f"drop_rate={self.drop_rate}, drop_type={self.drop_type}"
