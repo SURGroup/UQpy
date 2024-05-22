d65483bc502855b8ba7ced3be37e408e3279c5bb
connor-krill <ckrill1@jh.edu>
2024-05-22 13:09:41 -0400

    improved documentation, imports, and beartype

--- src/UQpy/distributions/collection/Normal.py
+++ src/UQpy/distributions/collection/Normal.py
@@ -2,14 +2,13 @@ from typing import Union
 import numpy as np
 import scipy.stats as stats
 from beartype import beartype
-from scipy.special import erf, erfinv
+from scipy.special import erf
 from UQpy.distributions.baseclass import DistributionContinuous1D
 from UQpy.utilities.ValidationTypes import NumericArrayLike
 
 
+@beartype
 class Normal(DistributionContinuous1D):
-
-    @beartype
     def __init__(
         self, loc: Union[None, float, int] = 0.0, scale: Union[None, float, int] = 1.0
     ):
@@ -23,32 +22,36 @@ class Normal(DistributionContinuous1D):
         self.pdf = self.__probability_density_function
         self.cdf = self.__cumulative_distribution_function
 
-    @beartype
-    def __probability_density_function(self, x: NumericArrayLike):
+    def __probability_density_function(
+        self, x: NumericArrayLike
+    ) -> Union[float, np.ndarray]:
         """Probability density function for normal distribution
 
-        :param x:
-        :return:
+        :param x: Points to evaluate pdf at
+        :return: PDF of ``x`` as defined by :math:`f_x(x)`
         """
         x_array = np.atleast_1d(x)
-        mean = self.parameters['loc']
-        standard_deviation = self.parameters['scale']
+        mean = self.parameters["loc"]
+        standard_deviation = self.parameters["scale"]
         normalizing_constant = 1 / (standard_deviation * np.sqrt(2 * np.pi))
-        pdf = normalizing_constant * np.exp(-0.5 * ((x_array - mean) / standard_deviation)**2)
+        pdf = normalizing_constant * np.exp(
+            -0.5 * ((x_array - mean) / standard_deviation) ** 2
+        )
         if isinstance(x, int) or isinstance(x, float):
             return pdf[0]
         return pdf
 
-    @beartype
-    def __cumulative_distribution_function(self, x: NumericArrayLike):
+    def __cumulative_distribution_function(
+        self, x: NumericArrayLike
+    ) -> Union[float, np.ndarray]:
         """Cumulative distribution function for the normal distribution defined with the error function
 
-        :param x:
-        :return:
+        :param x: Points to evaluate cdf at
+        :return: CDF of ``x`` as defined by :math:`F_X(x)`
         """
         x_array = np.atleast_1d(x)
-        mean = self.parameters['loc']
-        standard_deviation = self.parameters['scale']
+        mean = self.parameters["loc"]
+        standard_deviation = self.parameters["scale"]
         erf_input = (x_array - mean) / (standard_deviation * np.sqrt(2.0))
         cdf = (1.0 + erf(erf_input)) / 2.0
         if isinstance(x, int) or isinstance(x, float):
--- src/UQpy/distributions/collection/Uniform.py
+++ src/UQpy/distributions/collection/Uniform.py
@@ -1,15 +1,13 @@
-from typing import Union
-
 import numpy as np
 import scipy.stats as stats
 from beartype import beartype
-
+from typing import Union
 from UQpy.distributions.baseclass import DistributionContinuous1D
 from UQpy.utilities.ValidationTypes import NumericArrayLike
+from line_profiler_pycharm import profile
 
-
+@beartype
 class Uniform(DistributionContinuous1D):
-    @beartype
     def __init__(
         self, loc: Union[None, float, int] = 0.0, scale: Union[None, float, int] = 1.0
     ):
@@ -25,16 +23,18 @@ class Uniform(DistributionContinuous1D):
         self.cdf = self.__cumulative_distribution_function
         self.icdf = self.__inverse_cumulative_distribution_function
 
-    @beartype
-    def __probability_density_function(self, x: NumericArrayLike):
+    @profile
+    def __probability_density_function(
+        self, x: NumericArrayLike
+    ) -> Union[float, np.ndarray]:
         """Probability Density Function for the uniform distribution
 
         :param x: Points at which to evaluate the probability density function
         :return: pdf at all points in x
         """
         x_array = np.atleast_1d(x)
-        loc = self.parameters['loc']
-        scale = self.parameters['scale']
+        loc = self.parameters["loc"]
+        scale = self.parameters["scale"]
         mask = (loc <= x_array) & (x_array <= loc + scale)
         pdf = np.zeros_like(x_array)
         pdf[mask] = 1 / scale
@@ -42,15 +42,18 @@ class Uniform(DistributionContinuous1D):
             return pdf[0]
         return pdf
 
-    @beartype
-    def __cumulative_distribution_function(self, x: NumericArrayLike):
+    @profile
+    def __cumulative_distribution_function(
+        self, x: NumericArrayLike
+    ) -> Union[float, np.ndarray]:
         """Cumulative Distribution Function for the Uniform Distribution
 
         :param x: Points at which to evaluate the cumulative distribution function
+        :return:  cdf of ``x`` as defined by :math:`F_X(x)`
         """
         x_array = np.atleast_1d(x)
-        loc = self.parameters['loc']
-        scale = self.parameters['scale']
+        loc = self.parameters["loc"]
+        scale = self.parameters["scale"]
         cdf = np.zeros_like(x_array)
         middle_mask = (loc < x_array) & (x_array < loc + scale)
         upper_mask = loc + scale <= x_array
@@ -60,15 +63,18 @@ class Uniform(DistributionContinuous1D):
             return cdf[0]
         return cdf
 
-    def __inverse_cumulative_distribution_function(self, x: NumericArrayLike):
+    @profile
+    def __inverse_cumulative_distribution_function(
+        self, x: NumericArrayLike
+    ) -> Union[float, np.ndarray]:
         """Inverse cumulative distribution function for uniform distribution
 
         :param x: Point at which to evaluate the inverse cumulative distribution function
-        :return:
+        :return: inverse cdf of ``x`` as defined by :math:`F^{-1}_X(x)`
         """
         x_array = np.atleast_1d(x)
-        loc = self.parameters['loc']
-        scale = self.parameters['scale']
+        loc = self.parameters["loc"]
+        scale = self.parameters["scale"]
         icdf = np.full(x_array.shape, np.nan)
         mask = (0 <= x_array) & (x_array <= 1)
         icdf[mask] = loc + (x_array[mask] * scale)
