298ccb6e6f9306c60227167c19ab1e2500193497
connor-krill <ckrill1@jh.edu>
2024-05-28 12:00:13 -0400

    added int as allowed output type

--- src/UQpy/distributions/collection/Uniform.py
+++ src/UQpy/distributions/collection/Uniform.py
@@ -27,7 +27,7 @@ class Uniform(DistributionContinuous1D):
     @profile
     def __probability_density_function(
         self, x: NumericArrayLike
-    ) -> Union[float, np.ndarray]:
+    ) -> Union[int, float, np.ndarray]:
         """Probability Density Function for the uniform distribution
 
         :param x: Points at which to evaluate the probability density function
@@ -36,17 +36,20 @@ class Uniform(DistributionContinuous1D):
         x_array = np.atleast_1d(x)
         loc = self.parameters["loc"]
         scale = self.parameters["scale"]
+        mask_zero_density = (x_array < loc) | (loc + scale < x_array)
         mask = (loc <= x_array) & (x_array <= loc + scale)
-        pdf = np.zeros_like(x_array)
+        pdf = np.full_like(x_array, np.nan)
+        pdf[mask_zero_density] = 0.0
         pdf[mask] = 1 / scale
+
         if isinstance(x, int) or isinstance(x, float):
-            return pdf[0]
+            return float(pdf[0])
         return pdf
 
     @profile
     def __cumulative_distribution_function(
         self, x: NumericArrayLike
-    ) -> Union[float, np.ndarray]:
+    ) -> Union[int, float, np.ndarray]:
         """Cumulative Distribution Function for the Uniform Distribution
 
         :param x: Points at which to evaluate the cumulative distribution function
@@ -55,19 +58,21 @@ class Uniform(DistributionContinuous1D):
         x_array = np.atleast_1d(x)
         loc = self.parameters["loc"]
         scale = self.parameters["scale"]
-        cdf = np.zeros_like(x_array)
+        cdf = np.full_like(x_array, np.nan)
+        lower_mask = x_array <= loc
         middle_mask = (loc < x_array) & (x_array < loc + scale)
         upper_mask = loc + scale <= x_array
+        cdf[lower_mask] = 0.0
         cdf[middle_mask] = (x_array[middle_mask] - loc) / scale
-        cdf[upper_mask] = 1
+        cdf[upper_mask] = 1.0
         if isinstance(x, int) or isinstance(x, float):
-            return cdf[0]
+            return float(cdf[0])
         return cdf
 
     @profile
     def __inverse_cumulative_distribution_function(
         self, x: NumericArrayLike
-    ) -> Union[float, np.ndarray]:
+    ) -> Union[int, float, np.ndarray]:
         """Inverse cumulative distribution function for uniform distribution
 
         :param x: Point at which to evaluate the inverse cumulative distribution function
@@ -76,9 +81,9 @@ class Uniform(DistributionContinuous1D):
         x_array = np.atleast_1d(x)
         loc = self.parameters["loc"]
         scale = self.parameters["scale"]
-        icdf = np.full(x_array.shape, np.nan)
+        icdf = np.full_like(x_array, np.nan)
         mask = (0 <= x_array) & (x_array <= 1)
         icdf[mask] = loc + (x_array[mask] * scale)
         if isinstance(x, int) or isinstance(x, float):
-            return icdf[0]
+            return float(icdf[0])
         return icdf
--- tests/unit_tests/distributions/test_uniform.py
+++ tests/unit_tests/distributions/test_uniform.py
@@ -10,9 +10,9 @@ uniform = Uniform()
 scipy_uniform = stats.uniform()
 
 
-@given(st.floats(allow_nan=False))
+@given(st.floats(allow_nan=True))
 def test_uniform_pdf_float(x):
-    """Test custom implementation of uniform pdf on float inputs. Should return flosa"""
+    """Test custom implementation of uniform pdf on float inputs"""
     pdf = uniform.pdf(x)
     scipy_pdf = scipy_uniform.pdf(x)
     assert isinstance(pdf, float)
@@ -20,14 +20,15 @@ def test_uniform_pdf_float(x):
 
 
 @given(array_shapes(min_dims=1, min_side=1))
-def test_uniform_pdf_array(x):
+def test_uniform_pdf_array(size):
+    x = np.random.normal(0, 1, size=size)
     pdf = uniform.pdf(x)
     scipy_pdf = scipy_uniform.pdf(x)
     assert isinstance(pdf, np.ndarray)
     assert np.allclose(pdf, scipy_pdf, equal_nan=True)
 
 
-@given(st.floats(allow_nan=False))
+@given(st.floats(allow_nan=True))
 def test_uniform_cdf_float(x):
     pdf = uniform.pdf(x)
     scipy_pdf = scipy_uniform.pdf(x)
@@ -36,14 +37,15 @@ def test_uniform_cdf_float(x):
 
 
 @given(array_shapes(min_dims=1, min_side=1))
-def test_uniform_cdf_array(x):
+def test_uniform_cdf_array(size):
+    x = np.random.normal(0, 1, size=size)
     pdf = uniform.pdf(x)
     scipy_pdf = scipy_uniform.pdf(x)
     assert isinstance(pdf, np.ndarray)
     assert np.allclose(pdf, scipy_pdf, equal_nan=True)
 
 
-@given(st.floats(allow_nan=False))
+@given(st.floats(allow_nan=True))
 def test_uniform_icdf_float(y):
     icdf = uniform.icdf(y)
     scipy_icdf = scipy_uniform.ppf(y)
@@ -52,8 +54,15 @@ def test_uniform_icdf_float(y):
 
 
 @given(array_shapes(min_dims=1, min_side=1))
-def test_uniform_icdf_array(y):
+def test_uniform_icdf_array(size):
+    y = np.random.normal(0, 1, size=size)
     icdf = uniform.icdf(y)
     scipy_icdf = scipy_uniform.ppf(y)
     assert isinstance(icdf, np.ndarray)
     assert np.allclose(icdf, scipy_icdf, equal_nan=True)
+
+
+@given(st.floats(allow_nan=True))  # FixMe: restrict these values between 0 and
+def test_uniform_icdf_cdf(x):
+    y = uniform.icdf(uniform.cdf(x))
+    assert np.allclose(x, y, equal_nan=True)
