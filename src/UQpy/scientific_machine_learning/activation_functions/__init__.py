b0ce46022a7c57b54933cae0ddf75d00e049a601
connor-krill <ckrill1@jh.edu>
2024-05-31 17:33:38 -0400

    deleted unneeded code

--- src/UQpy/scientific_machine_learning/activation_functions/__init__.py
+++ src/UQpy/scientific_machine_learning/activation_functions/__init__.py
@@ -1,9 +1,6 @@
 from UQpy.scientific_machine_learning.activation_functions.GaussianNormalizer import (
     GaussianNormalizer,
 )
-from UQpy.scientific_machine_learning.activation_functions.MinMaxNormalizer import (
-    MinMaxNormalizer,
-)
 from UQpy.scientific_machine_learning.activation_functions.RangeNormalizer import (
     RangeNormalizer,
 )
