0255886536703b94448acf66055349e010764ab6
connor-krill <ckrill1@jh.edu>
2024-05-31 17:31:23 -0400

    improved FNO architecture

--- src/UQpy/scientific_machine_learning/neural_networks/FourierNeuralOperator1D.py
+++ src/UQpy/scientific_machine_learning/neural_networks/FourierNeuralOperator1D.py
@@ -29,7 +29,9 @@ class FourierNeuralOperator1D(NeuralNetwork):
         self.modes = modes
         self.width = width
         self.padding = padding  # pad the domain if input is non-periodic
-        self.fc0 = nn.Linear(2, self.width)  # input channel is 2: (a(x), x)
+        self.fc0 = nn.Linear(
+            2, self.width
+        )  # input channel is 2: (a(x), x)
 
         self.conv0 = SpectralConv1d(self.width, self.width, self.modes)
         self.conv1 = SpectralConv1d(self.width, self.width, self.modes)
@@ -45,8 +47,8 @@ class FourierNeuralOperator1D(NeuralNetwork):
         self.fc2 = nn.Linear(32, 6)
 
     def forward(self, x):
-        grid = self.get_grid(x.shape, x.device)
-        x = torch.cat((x, grid), dim=-1)
+        # grid = self.get_grid(x.shape, x.device)
+        # x = torch.cat((x, grid), dim=-1)
         x = self.fc0(x)
         x = x.permute(0, 2, 1)
         if self.padding > 0:
@@ -70,17 +72,20 @@ class FourierNeuralOperator1D(NeuralNetwork):
         x1 = self.conv3(x)
         x2 = self.w3(x)
         x = x1 + x2
+        # todo: note last FNO does not have activation
 
         if self.padding > 0:
             x = x[..., : -self.padding]  # pad the domain if input is non-periodic
         x = x.permute(0, 2, 1)
-        x = self.fc1(x)
+
+        x = self.fc1(x)  # ToDo: User should be allowed to place constraints
         x = F.relu(x)
-        x = self.fc2(x)
+
+        x = self.fc2(x)  # ToDo: default linear always applied
         return x
 
-    def get_grid(self, shape, device):
-        batchsize, size_x = shape[0], shape[1]
-        gridx = torch.tensor(torch.linspace(0, 1, size_x), dtype=torch.float)
-        gridx = gridx.reshape(1, size_x, 1).repeat([batchsize, 1, 1])
-        return gridx.to(device)
+    # def get_grid(self, shape, device):
+    #     batchsize, size_x = shape[0], shape[1]
+    #     gridx = torch.tensor(torch.linspace(0, 1, size_x), dtype=torch.float)
+    #     gridx = gridx.reshape(1, size_x, 1).repeat([batchsize, 1, 1])
+    #     return gridx.to(device)
