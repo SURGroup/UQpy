38bc9d52c42b885e2aa7937299e43ab9d9a7a6e2
connor-krill <ckrill1@jh.edu>
2024-06-04 10:49:07 -0400

    removed activation function

--- src/UQpy/scientific_machine_learning/layers/FourierBlock1d.py
+++ src/UQpy/scientific_machine_learning/layers/FourierBlock1d.py
@@ -2,41 +2,40 @@ import torch
 import torch.nn as nn
 import UQpy.scientific_machine_learning as sml
 from UQpy.scientific_machine_learning.baseclass import Layer
-from typing import Union
+from UQpy.utilities.ValidationTypes import PositiveInteger
 from beartype import beartype
 
 
 @beartype
 class FourierBlock1d(Layer):
 
-    def __init__(self, width, modes, activation: Union[None, nn.Module] = nn.ReLU()):
-        """
+    def __init__(self, width: PositiveInteger, modes: PositiveInteger):
+        """Fourier block for Fourier Neural Operator. Computes ``spectral_convolution(x) + convolution(x)``
 
-        :param width:
+        :param width: Number of in and out channels
         :param modes:
         """
         super().__init__()
         self.width = width
         self.modes = modes
-        self.activation = activation
+
         self.spectral_conv = sml.SpectralConv1d(self.width, self.width, self.modes)
+        """Spectral convolution defined by ``UQpy.sml.SpectralConv1d(in_channels=width, out_channels=width, modes=modes)"""
         self.conv = nn.Conv1d(self.width, self.width, 1)
+        """Convolution layer defined by ``torch.nn.Conv1d(in_channels=width, out_channels=with, kernel_size=1)``"""
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
 
-        :param x:
-        :return:
+        :param x: Input tensor
+        :return: Output tensor
         """
-        y1 = self.spectral_conv(x)
-        y2 = self.conv(x)
-        if self.activation is None:
-            return y1 + y2
-        else:
-            return self.activation(y1 + y2)
+        return self.spectral_conv(x) + self.conv(x)
 
     def extra_repr(self) -> str:
-        s = f"width={self.width}, modes={self.modes}"
-        if self.activation is not nn.ReLU():
-            s += f", activation={self.activation}"
-        return s
+        return f"width={self.width}, modes={self.modes}"
+
+
+if __name__ == "__main__":
+    lay = sml.BayesianLinear(4, 5)
+    print(lay.sampling)
