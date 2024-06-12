a0e5bd04b06a0c0f5d043e36f677340ebf3205c6
connor-krill <ckrill1@jh.edu>
2024-06-12 12:23:50 -0400

    reformatted FNO to consistency

--- src/UQpy/scientific_machine_learning/layers/Fourier1d.py
+++ src/UQpy/scientific_machine_learning/layers/Fourier1d.py
@@ -1,13 +1,31 @@
+import torch
 import torch.nn as nn
 import UQpy.scientific_machine_learning as sml
-from UQpy.scientific_machine_learning.baseclass import FourierLayer
+from UQpy.scientific_machine_learning.baseclass import Layer
+from UQpy.utilities.ValidationTypes import PositiveInteger
 
 
-class Fourier1d(FourierLayer):
-    @property
-    def spectral_conv(self):
-        return sml.SpectralConv1d(self.width, self.width, self.modes)
+class Fourier1d(Layer):
+    def __init__(self, width: PositiveInteger, modes: PositiveInteger, **kwargs):
+        """Construct a Fourier block to compute :math:`\mathcal{F}^{-1} (R (\mathcal{F}x))`
 
-    @property
-    def conv(self):
-        return nn.Conv1d(self.width, self.width, 1)
+        :param width: Number of neurons in the layer
+        :param modes: Number of Fourier modes to keep
+        """
+        super().__init__(**kwargs)
+        self.width = width
+        self.modes = modes
+
+        self.conv = nn.Conv1d(self.width, self.width, 1)
+        self.spectral_conv = sml.SpectralConv1d(self.width, self.width, self.modes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+
+        :param x:
+        :return:
+        """
+        return self.spectral_conv(x) + self.conv(x)
+
+    def extra_repr(self) -> str:
+        return f"width={self.width}, modes={self.modes}"
