666d9a3365a2aa92b574f4b16c9cef6b4bf09639
connor-krill <ckrill1@jh.edu>
2024-06-13 17:28:43 -0400

    moved computation to functional

--- src/UQpy/scientific_machine_learning/functional/__init__.py
+++ src/UQpy/scientific_machine_learning/functional/__init__.py
@@ -1,3 +1,4 @@
+from UQpy.scientific_machine_learning.functional.gaussian_kullback_leiber_loss import gaussian_kullback_leiber_loss
 from UQpy.scientific_machine_learning.functional.spectral_conv1d import spectral_conv1d
-# from UQpy.scientific_machine_learning.functional.spectral_conv2d import spectral_conv2d
+from UQpy.scientific_machine_learning.functional.spectral_conv2d import spectral_conv2d
 from UQpy.scientific_machine_learning.functional.spectral_conv3d import spectral_conv3d
--- src/UQpy/scientific_machine_learning/layers/BayesianSpectralConv1d.py
+++ src/UQpy/scientific_machine_learning/layers/BayesianSpectralConv1d.py
@@ -1,5 +1,5 @@
 import torch
-import torch.nn as nn
+import UQpy.scientific_machine_learning.functional as func
 from UQpy.scientific_machine_learning.baseclass import BayesianLayer
 from UQpy.utilities.ValidationTypes import PositiveInteger
 
@@ -13,7 +13,7 @@ class BayesianSpectralConv1d(BayesianLayer):
         modes: PositiveInteger,
         priors=None,
         sampling=True,
-        **kwargs
+        **kwargs,
     ):
         weight_shape = (in_channels, out_channels, modes)
         bias_shape = None
@@ -22,33 +22,22 @@ class BayesianSpectralConv1d(BayesianLayer):
         self.out_channels = out_channels
         self.modes = modes
 
-        self.scale: float = 1 / (self.in_channels * self.out_channels)
+        self.scale: float = 1 / (self.in_channels * self.out_channels)  # ToDo: Include in priors?
         """Scalar normalizing factor for weights"""
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Compute Fourier coefficients up to factor of :math:`e^{\\text{-some constant}}`
 
-        :param x: Tensor of shape (n_batch, in_channels, n_x)
-        :return: Tensor of shape (n_batch, out_channels, n_x)
+        :param x: Tensor of shape :math:`(N, C_\\text{in}, L)`
+        :return: Tensor of shape :math:`(N, C_\\text{out}, L)`
         """
-        batch_size = x.shape[0]
-        x_ft = torch.fft.rfft(x)
-
-        out_ft = torch.zeros(
-            batch_size,
-            self.out_channels,
-            x.size(-1) // 2 + 1,
-            dtype=torch.cfloat,
-        )  # multiply relevant Fourier modes
-
         weights, _ = self.get_weight_bias()
-        weights *= self.scale
-        out_ft[:, :, : self.modes] = self.complex_multiplication(
-            x_ft[:, :, : self.modes], weights
-        )
-
-        x = torch.fft.irfft(out_ft, n=x.size(-1))  # return to physical space
-        return x
+        return func.spectral_conv1d(x, weights, self.out_channels, self.modes)
 
     def extra_repr(self) -> str:
-        return f"in_channels={self.in_channels}, out_channels={self.out_channels}, modes={self.modes}"
+        return (
+            f"in_channels={self.in_channels},"
+            f" out_channels={self.out_channels},"
+            f" modes={self.modes},"
+            f" sampling={self.sampling}"
+        )
