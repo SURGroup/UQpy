28f563183cd900ca03ac048c01b3cea2c7b9c4a5
connor-krill <ckrill1@jh.edu>
2024-06-26 16:05:42 -0400

    standardized usage, variables, and documentation of Fourier layers

---
+++ tests/unit_tests/scientific_machine_learning/layers/test_fourier3d.py
@@ -0,0 +1,25 @@
+import torch
+import UQpy.scientific_machine_learning as sml
+from hypothesis import given, settings, strategies as st
+from hypothesis.extra.numpy import array_shapes
+
+
+@settings(deadline=1_000)
+@given(
+    batch_size=st.integers(min_value=1, max_value=2),
+    width=st.integers(min_value=1, max_value=8),
+    signal_shape=array_shapes(min_dims=3, max_dims=3, min_side=64, max_side=128),
+    modes=array_shapes(min_dims=3, max_dims=3, min_side=1, max_side=33),
+)
+def test_output_shape(batch_size, width, signal_shape, modes):
+    """Fourier3d takes in a tensor (batch_size, width, H, W, D) and outputs a tensor of the same shape"""
+    H, W, D = signal_shape
+    x = torch.rand((batch_size, width, H, W, D))
+    fourier = sml.Fourier3d(width, modes)
+    y = fourier(x)
+    assert x.shape == y.shape
+
+
+def test_extra_repr():
+    fourier = sml.Fourier3d(4, (8, 16, 32))
+    assert fourier.__str__() == "Fourier3d(width=4, modes=(8, 16, 32))"
