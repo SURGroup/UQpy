89d54e8e28ce93d1035f1dc24203bbad27fdc661
connor-krill <ckrill1@jh.edu>
2024-07-05 15:19:20 -0400

    cleaned up file structure

--- src/UQpy/scientific_machine_learning/__init__.py
+++ src/UQpy/scientific_machine_learning/__init__.py
@@ -1,10 +1,4 @@
-from UQpy.scientific_machine_learning.activations import (
-    Dropout,
-    Dropout1d,
-    Dropout2d,
-    Dropout3d,
-    Permutation,
-)
+from UQpy.scientific_machine_learning.activations import *
 from UQpy.scientific_machine_learning.layers import *
 from UQpy.scientific_machine_learning.losses import *
 from UQpy.scientific_machine_learning.neural_networks import (
@@ -13,4 +7,4 @@ from UQpy.scientific_machine_learning.neural_networks import (
     FourierNeuralOperator,
     UNeuralNetwork,
 )
-from UQpy.scientific_machine_learning.trainers import Trainer, BBBTrainer, HMCTrainer
+from UQpy.scientific_machine_learning.trainers import *
--- src/UQpy/scientific_machine_learning/layers/__init__.py
+++ src/UQpy/scientific_machine_learning/layers/__init__.py
@@ -1,15 +1,12 @@
 from UQpy.scientific_machine_learning.layers.BayesianConv1d import BayesianConv1d
 from UQpy.scientific_machine_learning.layers.BayesianConv2d import BayesianConv2d
 from UQpy.scientific_machine_learning.layers.BayesianLinear import BayesianLinear
-from UQpy.scientific_machine_learning.layers.BayesianSpectralConv1d import BayesianSpectralConv1d
 from UQpy.scientific_machine_learning.layers.BayesianFourier1d import BayesianFourier1d
 from UQpy.scientific_machine_learning.layers.Fourier1d import Fourier1d
 from UQpy.scientific_machine_learning.layers.Fourier2d import Fourier2d
 from UQpy.scientific_machine_learning.layers.Fourier3d import Fourier3d
 from UQpy.scientific_machine_learning.layers.GaussianNormalizer import GaussianNormalizer
+from UQpy.scientific_machine_learning.layers.RangeNormalizer import RangeNormalizer
 from UQpy.scientific_machine_learning.layers.ProbabilisticLayer import (
     ProbabilisticLayer,
 )
-from UQpy.scientific_machine_learning.layers.SpectralConv1d import SpectralConv1d
-from UQpy.scientific_machine_learning.layers.SpectralConv2d import SpectralConv2d
-from UQpy.scientific_machine_learning.layers.SpectralConv3d import SpectralConv3d
