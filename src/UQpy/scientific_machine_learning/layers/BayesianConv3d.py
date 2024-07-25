2069024870e10b6332b07b12a9b7f349aa49142e
nlbetters <nickbetters2@gmail.com>
2024-07-25 14:41:38 -0400

    Initial commit of BayesianConv3d.py

---
+++ src/UQpy/scientific_machine_learning/layers/BayesianConv3d.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn.functional as F
+from torch.nn.modules.utils import _triple
+from typing import Union
+from UQpy.scientific_machine_learning.baseclass import BayesianLayer
+from UQpy.utilities.ValidationTypes import PositiveInteger, NonNegativeInteger
+
+
+
+class BayesianConv3d(BayesianLayer):
+
+    def __init__(
+        self,
+        in_channels: PositiveInteger,
+        out_channels: PositiveInteger,
+        kernel_size: Union[PositiveInteger, tuple[PositiveInteger, PositiveInteger, PositiveInteger]],
+        stride: Union[PositiveInteger, tuple[PositiveInteger, PositiveInteger, PositiveInteger]] = 1,
+        padding: Union[
+            str, NonNegativeInteger, tuple[NonNegativeInteger, NonNegativeInteger, NonNegativeInteger]
+        ] = 0,
+        dilation: Union[PositiveInteger, tuple[PositiveInteger, PositiveInteger, PositiveInteger]] = 1,
+        groups: PositiveInteger = 1,
+        bias: bool = True,
+        priors: dict = None,
+        sampling: bool = True,
+        **kwargs
+    ):
+
+        """ Applies a Bayesian 3D convolution over an input signal composed of several input planes.
+
+        :param in_channels: Number of channels in the input image
+        :param out_channels: Number of channels produced by the convolution
+        :param kernel_size: Size of the convolving kernel
+        :param stride: Stride of the convolution. Default: 1
+        :param padding: Padding added to all six sides of the input. Default: 0 todo might need to change notation
+        :param dilation: Spacing between kernel elements. Default: 1
+        :param groups: Number of blocked connections from input channels to output channels. Default: 1.
+         ``in_channels`` and ``out_channels`` must both be divisible by ``groups``.
+        :param bias: If True, adds a learnable bias to the output. Default: True
+        :param priors: Prior and posterior distribution parameters. The dictionary keys and their default values are:
+
+         - ``priors["prior_mu"]`` = :math:`0`
+         - ``priors["prior_sigma"]`` = :math:`0.1`
+         - ``priors["posterior_mu_initial"]`` = ``(0, 0.1)``
+         - ``priors["posterior_rho_initial"]`` = ``(-3, 0.1)``
+        :param sampling: If ``True``, sample layer parameters from their respective Gaussian distributions.
+         If ``False``, use distribution mean as parameter values.
+
+        Note: This class calls ``torch.nn.functional.conv3d`` with ``padding_mode='zeros'``.
+
+
+        Shape:
+
+        - Input: :math:`(N, C_\text{in},D_\text{in}, H_\text{in}, W_\text{in})` or :math:`(C_\text{in},D_\text{in}, H_\text{in}, W_\text{in})`
+        - Output: :math:`(N, C_\text{out},D_\text{out}, H_\text{out}, W_\text{out})` or :math:`(C_\text{out},D_\text{out}, H_\text{out}, W_\text{out})`
+
+        where :math:`D_\text{out} = \left\lfloor \frac{D_\text{in} + 2 \times \text{padding[0]} - \text{dilation[0]} \times (\text{kernel\_size[0] - 1}) - 1}{\text{stride[0]}} + 1\right\rfloor`
+
+        :math:`H_\text{out} = \left\lfloor \frac{H_\text{in} + 2 \times \text{padding[0]} - \text{dilation[0]} \times (\text{kernel\_size[0] - 1}) - 1}{\text{stride[0]}} + 1\right\rfloor`
+
+        :math:`W_\text{out} = \left\lfloor \frac{W_\text{in} + 2 \times \text{padding[1]} - \text{dilation[1]} \times (\text{kernel\_size[1] - 1}) - 1}{\text{stride[1]}} + 1\right\rfloor`
+
+        Example: todo NEED TO MAKE THIS EXAMPLE TEXT DIFFERENT COLOR       todo MAKE SURE THE EXAMPLE IS COMPLETE
+
+        # >>> # With cubic kernels and equal stride
+        # >>> Layer = nn.BayesianConv3d(16, 33, 3, stride=2)
+        # >>> # non-cubic kernels and unequal stride and with padding
+        # >>> Layer = nn.BayesianConv3d(16, 33, (3, 5, 2), stride=(2, 1, 1), padding=(4, 2, 0))
+        # >>> input = torch.randn(20, 16, 10, 50, 100)
+        # >>> output = m(input)
+
+        """
+        kernel_size = _triple(kernel_size)
+        weight_shape = (out_channels, in_channels // groups, kernel_size[0], kernel_size[1], kernel_size[2])
+        bias_shape = out_channels if bias else None
+        super().__init__(weight_shape, bias_shape, priors, sampling, **kwargs)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = _triple(kernel_size)
+        self.stride = _triple(stride)
+        self.padding = padding if isinstance(padding, str) else _triple(padding)
+        self.dilation = _triple(dilation)
+        self.groups = groups
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """ Apply ``F.conv3d`` to ``x`` where the weight and bias are drawn from random variables
+
+        :param x: Tensor of shape :math:`(N, C_\text{in}, D_\text{in}, H_\text{in}, W_\text{in})`
+        :return: Tensor of shape :math:`(N, C_\text{out}, D_\text{out}, H_\text{out}, W_\text{out})`
+        """
+        weight, bias = self.get_weight_bias()
+        return F.conv3d(
+            x,
+            weight,
+            bias,
+            stride=self.stride,
+            padding=self.padding,
+            dilation=self.dilation,
+            groups=self.groups,
+        )
+
+    def extra_repr(self) -> str:
+        s = "{in_channels}, {out_channels}, kernel_size={kernel_size}"
+        if self.stride != (1,) * len(tuple(self.stride)):
+            s += ", stride={stride}"
+        if self.padding != (0,) * len(tuple(self.padding)):
+            s += ", padding={padding}"
+        if self.dilation != (1,) * len(tuple(self.dilation)):
+            s += ", dilation={dilation}"
+        if self.groups != 1:
+            s += ", groups={groups}"
+        if self.bias is False:
+            s += ", bias={bias}"
+        if self.priors:
+            s += ", priors={priors}"
+        if not self.sampling:
+            s += ", sampling={sampling}"
+        return s.format(**self.__dict__)
