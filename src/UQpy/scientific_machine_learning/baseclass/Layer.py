1e85f0236030054b5801a99849b81345ea48c851
connor-krill <ckrill1@jh.edu>
2024-07-26 16:48:48 -0400

    added reset_parameters for standardization

--- src/UQpy/scientific_machine_learning/baseclass/Layer.py
+++ src/UQpy/scientific_machine_learning/baseclass/Layer.py
@@ -7,6 +7,11 @@ class Layer(nn.Module, ABC):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def reset_parameters(self, a, b):
+        """Fill all parameters with samples from :math:`\mathcal{U}(a, b)`"""
+        for p in self.parameters():
+            p.uniform_(a, b)
+
     @abstractmethod
     def forward(self, x: torch.Tensor) -> torch.Tensor: ...
 
