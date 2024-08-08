21211745f1aa4559e44b9d6264032dd9942e2e4f
connor-krill <ckrill1@jh.edu>
2024-08-08 14:33:58 -0400

    simplified keywords and for loop, added device

--- src/UQpy/scientific_machine_learning/losses/GaussianJensenShannonDivergence.py
+++ src/UQpy/scientific_machine_learning/losses/GaussianJensenShannonDivergence.py
@@ -8,15 +8,22 @@ from beartype import beartype
 @beartype
 class GaussianJensonShannonDivergence(Loss):
 
-    def __init__(self, reduction: str = "sum", **kwargs):
+    def __init__(self, reduction: str = "sum", device=None):
         """Analytic form for Gaussian JS divergence for all Bayesian layers in a module
 
-        :param reduction: Specifies the reduction to apply to the output: 'none', 'mean', or 'sum'.
-         'none': no reduction will be applied, 'mean': the output will be averaged, 'sum': the output will be summed.
-         Default: 'sum'
+        :param reduction: Specifies the reduction to apply to the output: 'mean' or 'sum'.
+         'mean': the output will be averaged, 'sum': the output will be summed. Default: 'sum'
         """
-        super().__init__(**kwargs)
+        super().__init__()
+        if reduction is "none":
+            raise ValueError(
+                "UQpy: GaussianJensonShannonDivergence does not accept reduction='none'. "
+                "Must be 'sum' or 'mean'."
+                "\nWe are deeply sorry this is inconsistent with the behavior of gaussian_jenson_shannon_divergence, "
+                "but we had no other choice."
+            )
         self.reduction = reduction
+        self.device = device
 
     def forward(self, network: nn.Module) -> torch.Tensor:
         """Compute the Gaussian JS divergence on all Bayesian layers in a module
@@ -24,24 +31,23 @@ class GaussianJensonShannonDivergence(Loss):
         :param network: Network containing Bayesian layers
         :return: Gaussian JS divergence between prior and posterior distributions
         """
-        divergence = torch.tensor(0.0, dtype=torch.float)
+        divergence = torch.tensor(0.0, dtype=torch.float, device=self.device)
         for layer in network.modules():
-            if isinstance(layer, BayesianLayer):
+            if not isinstance(layer, BayesianLayer):
+                continue
+            for name in layer.parameter_shapes:
+                if layer.parameter_shapes[name] is None:
+                    continue
+                posterior_mu = getattr(layer, f"{name}_mu")
+                rho = getattr(layer, f"{name}_rho")
+                posterior_sigma = torch.log1p(torch.exp(rho))
                 divergence += func.gaussian_jenson_shannon_divergence(
-                    layer.weight_mu,
-                    torch.log1p(torch.exp(layer.weight_sigma)),
-                    layer.prior_mu,
-                    layer.prior_sigma,
+                    posterior_mu,
+                    posterior_sigma,
+                    torch.tensor(layer.prior_mu, device=self.device),
+                    torch.tensor(layer.prior_sigma, device=self.device),
                     reduction=self.reduction,
                 )
-                if layer.bias:
-                    divergence += func.gaussian_jenson_shannon_divergence(
-                        layer.bias_mu,
-                        torch.log1p(torch.exp(layer.bias_sigma)),
-                        layer.prior_mu,
-                        layer.prior_sigma,
-                        reduction=self.reduction,
-                    )
         return divergence
 
     def extra_repr(self) -> str:
