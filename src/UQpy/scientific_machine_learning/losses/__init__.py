6766838081bb5d5e7f1f3a937ef72f10c8f4d71f
connor-krill <ckrill1@jh.edu>
2024-08-09 15:23:15 -0400

    deleted gaussianJS, replaced it with GeneralizedJensonShannonDivergence.py

--- src/UQpy/scientific_machine_learning/losses/__init__.py
+++ src/UQpy/scientific_machine_learning/losses/__init__.py
@@ -1,16 +1,19 @@
 from UQpy.scientific_machine_learning.losses.EvidenceLowerBound import (
     EvidenceLowerBound,
 )
-from UQpy.scientific_machine_learning.losses.GaussianJensenShannonDivergence import (
-    GaussianJensonShannonDivergence,
-)
+
 from UQpy.scientific_machine_learning.losses.GaussianKullbackLeiblerDivergence import (
     GaussianKullbackLeiblerDivergence,
 )
 from UQpy.scientific_machine_learning.losses.GeometricJensenShannonDivergence import (
     GeometricJensenShannonDivergence,
 )
-from UQpy.scientific_machine_learning.losses.MCKullbackLieblerDivergence import MCKullbackLeiblerDivergence
+from UQpy.scientific_machine_learning.losses.GeneralizedJensonShannonDivergence import (
+    GeneralizedJensonShannonDivergence,
+)
+from UQpy.scientific_machine_learning.losses.MCKullbackLieblerDivergence import (
+    MCKullbackLeiblerDivergence,
+)
 from UQpy.scientific_machine_learning.losses.LpLoss import LpLoss
 from UQpy.scientific_machine_learning.losses.PhysicsInformedLoss import (
     PhysicsInformedLoss,
