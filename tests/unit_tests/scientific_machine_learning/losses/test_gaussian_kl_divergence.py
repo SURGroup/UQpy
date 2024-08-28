0cf78d8557aedee8e42c6d04e8280ffb48dd89e0
connor-krill <ckrill1@jh.edu>
2024-08-28 16:22:40 -0400

    improved existing test, added device test

--- tests/unit_tests/scientific_machine_learning/losses/test_gaussian_kl_divergence.py
+++ tests/unit_tests/scientific_machine_learning/losses/test_gaussian_kl_divergence.py
@@ -5,8 +5,6 @@ import UQpy.scientific_machine_learning as sml
 from hypothesis import given, strategies as st
 
 
-# ToDo: write device test
-
 @given(width=st.integers(min_value=1, max_value=10))
 def test_reduction_shape(width):
     """With the default reduction='sum', the divergence should be a scalar"""
@@ -27,3 +25,17 @@ def test_reduction_none():
     """Only accepts 'mean' or 'sum' as reduction"""
     with pytest.raises(ValueError):
         sml.GaussianKullbackLeiblerDivergence(reduction="none")
+
+
+def test_device():
+    """Note if neither cuda nor mps is available, this test will always pass"""
+    device = (
+        torch.device("cuda", 0)
+        if torch.cuda.is_available()
+        else torch.device("mps", 0) if torch.backends.mps.is_available() else "cpu"
+    )
+    model = sml.FeedForwardNeuralNetwork(sml.BayesianLinear(1, 1))
+    model.to(device)
+    divergence_function = sml.GaussianKullbackLeiblerDivergence(device=device)
+    divergence = divergence_function(model)
+    assert divergence.device == device
--- tests/unit_tests/scientific_machine_learning/losses/test_generalized_js_divergence.py
+++ tests/unit_tests/scientific_machine_learning/losses/test_generalized_js_divergence.py
@@ -1,27 +1,40 @@
-# ToDo: write this test with the new generalized JS in functional.
-# ToDo: write device test
-# import pytest
-# import torch
-# import torch.nn as nn
-# import UQpy.scientific_machine_learning as sml
-# from hypothesis import given, strategies as st
-#
-#
-# @given(width=st.integers(min_value=1, max_value=10))
-# def test_reduction_shape(width):
-#     network = nn.Sequential(
-#         sml.BayesianLinear(1, width),
-#         nn.ReLU(),
-#         sml.BayesianLinear(width, width),
-#         nn.ReLU(),
-#         sml.BayesianLinear(width, 1),
-#     )
-#     model = sml.FeedForwardNeuralNetwork(network)
-#     divergence_loss = sml.GeneralizedJensenShannonDivergence()
-#     divergence = divergence_loss(model)
-#     assert divergence.shape == torch.Size()
-#
-#
-# def test_reduction_none_raises_error():
-#     with pytest.raises(ValueError):
-#         sml.GeneralizedJensenShannonDivergence(reduction="sum")
+import pytest
+import torch
+import torch.nn as nn
+import UQpy as uq
+import UQpy.scientific_machine_learning as sml
+
+
+def test_reduction_shape():
+    width = 4
+    network = nn.Sequential(
+        sml.BayesianLinear(1, width),
+        nn.ReLU(),
+        nn.Linear(width, width),
+        nn.ReLU(),
+        sml.BayesianLinear(width, 1),
+    )
+    model = sml.FeedForwardNeuralNetwork(network)
+    divergence_function = sml.GeneralizedJensenShannonDivergence(uq.Normal, uq.Uniform)
+    divergence = divergence_function(model)
+    assert divergence.shape == torch.Size()
+
+
+def test_reduction_none_raises_error():
+    with pytest.raises(ValueError):
+        sml.GeneralizedJensenShannonDivergence(uq.Normal, uq.Normal, reduction="none")
+
+
+def test_device():
+    device = (
+        torch.device("cuda", 0)
+        if torch.cuda.is_available()
+        else torch.device("mps", 0) if torch.backends.mps.is_available() else "cpu"
+    )
+    divergence_function = sml.GeneralizedJensenShannonDivergence(
+        uq.Normal, uq.Normal, device=device
+    )
+    model = sml.FeedForwardNeuralNetwork(sml.BayesianLinear(1, 1))
+    model.to(device)
+    divergence = divergence_function(model)
+    assert divergence.device == device
