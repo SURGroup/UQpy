bc7de26ac304b3d2f9c0341159915d306a5f0e7e
connor-krill <ckrill1@jh.edu>
2024-10-14 17:49:45 -0400

    specific package versions for consistency with requirements.txt

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     package_data={"": ["*.pdf"]},
     python_requires='>3.9.0',
     install_requires=[
-        "numpy", "scipy>=1.6.0", "matplotlib", "scikit-learn", 'fire',
+        "numpy==1.26.4", "scipy>=1.6.0", "matplotlib==3.8.4", "scikit-learn==1.4.2", 'fire==0.6.0',
         "beartype==0.18.5",
     ],
     extras_require={
