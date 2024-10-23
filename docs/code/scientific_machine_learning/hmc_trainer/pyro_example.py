1f2de4cc5337b9a7234adf1f511a4482558f70dd
connor-krill <ckrill1@jh.edu>
2024-10-23 10:03:55 -0400

    applied black formatting

--- docs/code/scientific_machine_learning/hmc_trainer/pyro_example.py
+++ docs/code/scientific_machine_learning/hmc_trainer/pyro_example.py
@@ -1,4 +1,3 @@
-
 # %%
 from pyro.infer import Predictive
 from pyro.infer import MCMC, NUTS
@@ -16,7 +15,12 @@ np.random.seed(42)
 # Generate data
 x_obs = np.hstack([np.linspace(-0.2, 0.2, 500), np.linspace(0.6, 1, 500)])
 noise = 0.02 * np.random.randn(x_obs.shape[0])
-y_obs = x_obs + 0.3 * np.sin(2 * np.pi * (x_obs + noise)) + 0.3 * np.sin(4 * np.pi * (x_obs + noise)) + noise
+y_obs = (
+    x_obs
+    + 0.3 * np.sin(2 * np.pi * (x_obs + noise))
+    + 0.3 * np.sin(4 * np.pi * (x_obs + noise))
+    + noise
+)
 
 x_true = np.linspace(-0.5, 1.5, 1000)
 y_true = x_true + 0.3 * np.sin(2 * np.pi * x_true) + 0.3 * np.sin(4 * np.pi * x_true)
@@ -27,8 +31,8 @@ ylims = [-1.5, 2.5]
 
 # Create plot
 fig, ax = plt.subplots(figsize=(10, 5))
-ax.plot(x_true, y_true, 'b-', linewidth=3, label="True function")
-ax.plot(x_obs, y_obs, 'ko', markersize=4, label="Observations")
+ax.plot(x_true, y_true, "b-", linewidth=3, label="True function")
+ax.plot(x_obs, y_obs, "ko", markersize=4, label="Observations")
 ax.set_xlim(xlims)
 ax.set_ylim(ylims)
 ax.set_xlabel("X", fontsize=30)
@@ -37,33 +41,37 @@ ax.legend(loc=4, fontsize=15, frameon=False)
 
 plt.show()
 plt.close()
+
+
 # %%
 class MyFirstBNN(PyroModule):
-    def __init__(self, in_dim=1, out_dim=1, hid_dim=5, prior_scale=10.):
+    def __init__(self, in_dim=1, out_dim=1, hid_dim=5, prior_scale=10.0):
         super().__init__()
 
         self.activation = nn.Tanh()  # or nn.ReLU()
-        self.layer1 = PyroModule[nn.Linear](
-            in_dim, hid_dim)  # Input to hidden layer
-        self.layer2 = PyroModule[nn.Linear](
-            hid_dim, out_dim)  # Hidden to output layer
+        self.layer1 = PyroModule[nn.Linear](in_dim, hid_dim)  # Input to hidden layer
+        self.layer2 = PyroModule[nn.Linear](hid_dim, out_dim)  # Hidden to output layer
 
         # Set layer parameters as random variables
-        self.layer1.weight = PyroSample(dist.Normal(
-            0., prior_scale).expand([hid_dim, in_dim]).to_event(2))
-        self.layer1.bias = PyroSample(dist.Normal(
-            0., prior_scale).expand([hid_dim]).to_event(1))
-        self.layer2.weight = PyroSample(dist.Normal(
-            0., prior_scale).expand([out_dim, hid_dim]).to_event(2))
-        self.layer2.bias = PyroSample(dist.Normal(
-            0., prior_scale).expand([out_dim]).to_event(1))
+        self.layer1.weight = PyroSample(
+            dist.Normal(0.0, prior_scale).expand([hid_dim, in_dim]).to_event(2)
+        )
+        self.layer1.bias = PyroSample(
+            dist.Normal(0.0, prior_scale).expand([hid_dim]).to_event(1)
+        )
+        self.layer2.weight = PyroSample(
+            dist.Normal(0.0, prior_scale).expand([out_dim, hid_dim]).to_event(2)
+        )
+        self.layer2.bias = PyroSample(
+            dist.Normal(0.0, prior_scale).expand([out_dim]).to_event(1)
+        )
 
     def forward(self, x, y=None):
         x = x.reshape(-1, 1)
         x = self.activation(self.layer1(x))
         mu = self.layer2(x).squeeze()
         # Infer the response noise
-        sigma = pyro.sample("sigma", dist.Gamma(.5, 1))
+        sigma = pyro.sample("sigma", dist.Gamma(0.5, 1))
 
         # Sampling model
         with pyro.plate("data", x.shape[0]):
@@ -96,12 +104,12 @@ mcmc.run(x_train, y_train)
 predictive = Predictive(model=model, posterior_samples=mcmc.get_samples())
 x_test = torch.linspace(xlims[0], xlims[1], 3000)
 preds = predictive(x_test)
-#%%
+# %%
 
 
 def plot_predictions(preds):
-    y_pred = preds['obs'].T.detach().numpy().mean(axis=1)
-    y_std = preds['obs'].T.detach().numpy().std(axis=1)
+    y_pred = preds["obs"].T.detach().numpy().mean(axis=1)
+    y_std = preds["obs"].T.detach().numpy().std(axis=1)
 
     fig, ax = plt.subplots(figsize=(10, 5))
     xlims = [-0.5, 1.5]
@@ -111,15 +119,21 @@ def plot_predictions(preds):
     plt.xlabel("X", fontsize=30)
     plt.ylabel("Y", fontsize=30)
 
-    ax.plot(x_true, y_true, 'b-', linewidth=3, label="true function")
-    ax.plot(x_obs, y_obs, 'ko', markersize=4, label="observations")
-    ax.plot(x_obs, y_obs, 'ko', markersize=3)
-    ax.plot(x_test, y_pred, '-', linewidth=3,
-            color="#408765", label="predictive mean")
-    ax.fill_between(x_test, y_pred - 2 * y_std, y_pred + 2 *
-                    y_std, alpha=0.6, color='#86cfac', zorder=5)
+    ax.plot(x_true, y_true, "b-", linewidth=3, label="true function")
+    ax.plot(x_obs, y_obs, "ko", markersize=4, label="observations")
+    ax.plot(x_obs, y_obs, "ko", markersize=3)
+    ax.plot(x_test, y_pred, "-", linewidth=3, color="#408765", label="predictive mean")
+    ax.fill_between(
+        x_test,
+        y_pred - 2 * y_std,
+        y_pred + 2 * y_std,
+        alpha=0.6,
+        color="#86cfac",
+        zorder=5,
+    )
 
     plt.legend(loc=4, fontsize=15, frameon=False)
+
 
 # %%
 plot_predictions(preds)
