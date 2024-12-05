000d4ed1c8279dc9da1a815f505d8cbe368a8632
connor-krill <ckrill1@jh.edu>
2024-12-05 15:11:16 -0500

    updated HMC example

--- docs/code/scientific_machine_learning/hmc_trainer/train_fno_with_hmc.py
+++ docs/code/scientific_machine_learning/hmc_trainer/train_fno_with_hmc.py
@@ -63,22 +63,56 @@ model = FourierNeuralOperator()
 x_train = torch.load("initial_conditions_train.pt")
 y_train = torch.load("burgers_solutions_train.pt")
 
+
+# generate the HMC samples
+
 # Set hyperparameters for network
 tau_list = []
-tau = 1.0  # /100. # iris 1/10
+
+tau = 100.0
 for w in model.parameters():
     tau_list.append(tau)
 tau_list = torch.tensor(tau_list)
-params_init = hamiltorch.util.flatten(model).clone()
 
+step_size = 1e-4
+num_steps_per_sample = int(torch.pi / (2 * step_size * tau))
+
+params_init = hamiltorch.util.flatten(model).clone()
 params_hmc = hamiltorch.sample_model(
     model,
     x_train,
     y_train,
     params_init=params_init,
     num_samples=100,
-    step_size=1e-3,
-    num_steps_per_sample=10,
+    step_size=step_size,
+    num_steps_per_sample=num_steps_per_sample,
     tau_list=tau_list,
     model_loss=nn.MSELoss(reduction="sum"),
 )
+torch.save(params_hmc, "params_hmc.pt")
+
+# Evaluate the HMC samples on testing data
+x_test = torch.load("initial_conditions_test.pt")
+y_test = torch.load("burgers_solutions_test.pt")
+params_hmc = torch.load("params_hmc.pt")
+predictions, log_prob_list = hamiltorch.predict_model(
+    model,
+    x=x_test[0:1],
+    y=y_test[0:1],
+    samples=params_hmc[:],
+    model_loss=nn.MSELoss(reduction="sum"),
+    tau_list=tau_list,
+)
+
+predictions = predictions.squeeze()
+mean_prediction = torch.mean(predictions, dim=0)
+spacial_x = torch.linspace(0, 1, 256)
+fig, ax = plt.subplots()
+ax.plot(spacial_x, y_test[0].squeeze(), color="tab:blue")
+ax.plot(spacial_x, mean_prediction, color="black")
+ax.plot(spacial_x, predictions.T, color="gray", alpha=0.3)
+ax.legend(["y", "Mean Prediction", "Ensemble Predictions"])
+ax.set_title("HMC Predictions")
+ax.set(xlabel="x", ylabel="y(x, 0.5)")
+
+plt.show()
