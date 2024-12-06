ef9ed1be29759b70691c786bfe41130b32ee93b5
connor-krill <ckrill1@jh.edu>
2024-12-06 13:04:57 -0500

    finalized hmc example

--- docs/code/scientific_machine_learning/hmc_trainer/burgers_fno.py
+++ docs/code/scientific_machine_learning/hmc_trainer/burgers_fno.py
@@ -1,14 +1,35 @@
-"""Training a Fourier Neural Operator with Hamiltorch's HMC
+r"""
+Learning the Burgers' Operator with HMC
+=======================================
 
-Adapting the Hamiltorch notebook found at https://github.com/AdamCobb/hamiltorch/blob/master/notebooks/hamiltorch_Bayesian_NN_example.ipynb
+In this example, we train a Fourier Neural Operator (FNO) using the implementation of Hamiltonian Monte Carlo (HMC)
+provided by the Hamiltorch (https://adamcobb.github.io/journal/hamiltorch.html) library.
+Our FNO learns the mapping :math:`y(x, 0) \mapsto y(x, 0.5)` where :math:`y(x,t)` is the solution to the
+Burgers' equation given by
+
+.. math:: \frac{\partial}{\partial t}u(x, t) + u(x, t) \frac{\partial}{\partial x} u(x, t) = \nu \frac{\partial^2}{\partial x^2} u(x,t)
+
+This example is adapted from this Hamiltorch example (https://github.com/AdamCobb/hamiltorch/blob/master/notebooks/hamiltorch_Bayesian_NN_example.ipynb)
 """
+
+# %% md
+# We learn the mapping from the initial condition :math:`y(x, 0)` to a point at time :math:`t=0.5`.
+# The training data for this example was computed separately and is available on GitHub.
+#
+# First, import the necessary modules.
+
+# %%
 
 import torch
 import hamiltorch
 import matplotlib.pyplot as plt
 import torch.nn as nn
-import torch.nn.functional as F
 import UQpy.scientific_machine_learning as sml
+
+# %% md
+# Below we define the architecture of our neural operator as subclasses of PyTorch objects.
+
+# %%
 
 
 class FourierNeuralOperator(nn.Module):
@@ -60,24 +81,34 @@ class FourierNeuralOperator(nn.Module):
 
 model = FourierNeuralOperator()
 
+
+# %% md
+# The training and testing data was created by solving the Burgers' equation using numerical integration.
+# The dataset used here are available on our GitHub.
+
+# %%
+
 x_train = torch.load("initial_conditions_train.pt")
 y_train = torch.load("burgers_solutions_train.pt")
 
+# %% md
+# Finally, we pass our model and the data off the Hamiltorch for the training.
+# Note that Hamiltorch does not modify the parameters of the ``model`` object.
+# Rather, it provides a list of parameters that have been sampled from the posterior distribution.
 
-# generate the HMC samples
+# %%
 
 # Set hyperparameters for network
 tau_list = []
-
 tau = 100.0
 for w in model.parameters():
     tau_list.append(tau)
 tau_list = torch.tensor(tau_list)
-
 step_size = 1e-4
 num_steps_per_sample = 100
-
 params_init = hamiltorch.util.flatten(model).clone()
+
+# generate the HMC samples
 params_hmc = hamiltorch.sample_model(
     model,
     x_train,
@@ -89,12 +120,21 @@ params_hmc = hamiltorch.sample_model(
     tau_list=tau_list,
     model_loss=nn.MSELoss(reduction="sum"),
 )
+# optional, save the HMC  parameters
 # torch.save(params_hmc, "params_hmc.pt")
+
+# %% md
+# That's the Fourier neural operator trained with HMC!
+# The rest of this notebook visualizes the predictions from the sampled parameters.
+
+# %%
+
+# optional, load the HMC parameters. `params_hmc.pt` is available on our GitHub
+params_hmc = torch.load("params_hmc.pt")
 
 # Evaluate the HMC samples on testing data
 x_test = torch.load("initial_conditions_test.pt")
 y_test = torch.load("burgers_solutions_test.pt")
-params_hmc = torch.load("params_hmc.pt")
 predictions, log_prob_list = hamiltorch.predict_model(
     model,
     x=x_test[0:1],
@@ -104,6 +144,7 @@ predictions, log_prob_list = hamiltorch.predict_model(
     tau_list=tau_list,
 )
 
+# plot the results
 predictions = predictions.squeeze()
 mean_prediction = torch.mean(predictions, dim=0)
 spacial_x = torch.linspace(0, 1, 256)
--- docs/source/scientific_machine_learning/trainers/hmc_trainer.rst
+++ docs/source/scientific_machine_learning/trainers/hmc_trainer.rst
@@ -1,4 +1,15 @@
-Hamiltonian Monte Carlo Trainer (HMCTrainer)
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Hamiltonian Monte Carlo Trainers
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-This is a placeholder for documentation on the HMCTrainer.
+UQpy does not have its own implementation of a Hamiltonian Monte Carlo (HMC) trainer.
+For using the HMC algorithm to train neural networks, we refer users to Pyro HMC (https://docs.pyro.ai/en/stable/mcmc.html#hmc)
+or Hamiltorch HMC (https://adamcobb.github.io/journal/hamiltorch.html). Both of these packages are based on PyTorch,
+although it may be simpler to implement ``UQpy.sml`` models Hamiltorch.
+
+Examples
+--------
+
+.. toctree::
+
+    FNO with Hamiltorch <../../auto_examples/scientific_machine_learning/hmc_trainer/burgers_fno>
+
