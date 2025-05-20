c028e4c709e1decb96d93c0322f61ca1836abed3
connor-krill <ckrill1@jh.edu>
2025-05-20 16:51:41 -0400

    replaced strict equality with numpy's allclose to accommodate for floating point error

--- tests/unit_tests/transformations/test_nataf.py
+++ tests/unit_tests/transformations/test_nataf.py
@@ -18,13 +18,13 @@ def test_wrong_distribution_in_list():
     dist1 = Normal(loc=0.0, scale=1.0)
     rx = np.array([[1.0, 0.0], [0.0, 1.0]])
     with pytest.raises(Exception):
-        assert Nataf(distributions=[dist1, 'Beta'], corr_x=rx)
+        assert Nataf(distributions=[dist1, "Beta"], corr_x=rx)
 
 
 def test_wrong_distribution():
     rx = np.array([[1.0, 0.0], [0.0, 1.0]])
     with pytest.raises(Exception):
-        assert Nataf(distributions='Normal', corr_x=rx)
+        assert Nataf(distributions="Normal", corr_x=rx)
 
 
 def test_identity_correlation_x_normal():
@@ -70,7 +70,8 @@ def test_non_identity_correlation_uniform_z():
     dist2 = Uniform(loc=0.0, scale=1.0)
     rx = np.array([[1.0, 0.8], [0.8, 1.0]])
     ntf_obj = Nataf(distributions=[dist1, dist2], corr_x=rx)
-    np.testing.assert_allclose(ntf_obj.corr_z, [[1., 0.8134732861515996], [0.8134732861515996, 1.]], rtol=1e-09)
+    expected_corr_z = np.array([[1.0, 0.8134732861515996], [0.8134732861515996, 1.0]])
+    assert np.allclose(ntf_obj.corr_z, expected_corr_z)
 
 
 def test_non_identity_correlation_uniform_x():
@@ -78,7 +79,8 @@ def test_non_identity_correlation_uniform_x():
     dist2 = Uniform(loc=0.0, scale=1.0)
     rz = np.array([[1.0, 0.8], [0.8, 1.0]])
     ntf_obj = Nataf(distributions=[dist1, dist2], corr_z=rz)
-    assert (ntf_obj.corr_x == [[1., 0.7859392826067285], [0.7859392826067285, 1.]]).all()
+    expected_corr_x = np.array([[1.0, 0.7859392826067285], [0.7859392826067285, 1.0]])
+    assert np.allclose(ntf_obj.corr_x, expected_corr_x)
 
 
 def test_attribute_h():
@@ -86,7 +88,8 @@ def test_attribute_h():
     dist2 = Normal(loc=0.0, scale=1.0)
     rz = np.array([[1.0, 0.8], [0.8, 1.0]])
     ntf_obj = Nataf(distributions=[dist1, dist2], corr_z=rz)
-    np.testing.assert_allclose(ntf_obj.H, [[1., 0.], [0.8, 0.6]], rtol=1e-09)
+    expected_H = np.array([[1.0, 0.0], [0.8, 0.6]])
+    assert np.allclose(ntf_obj.H, expected_H)
 
 
 def test_samples_x():
@@ -131,15 +134,12 @@ def test_samples_x_jxz2():
     ntf_obj = Nataf(distributions=[dist1, dist2])
     samples_x = np.array([[0.3, 1.2, 3.5], [0.2, 2.4, 0.9]]).T
     ntf_obj.run(samples_x=samples_x, jacobian=True)
-    g = []
-    for i in range(3):
-        if i == 0:
-            g.append((ntf_obj.jxz[i] == np.array([[1.6789373877365803, 0.0], [0.0, 2.577850090371836]])).all())
-        elif i == 1:
-            g.append((ntf_obj.jxz[i] == np.array([[0.6433491348614259, 0.0], [0.0, 1.1906381155257868]])).all())
-        else:
-            g.append((ntf_obj.jxz[i] == np.array([[0.5752207318528584, 0.0], [0.0, 0.958701219754764]])).all())
-    assert np.all(g)
+    expected_jxz = [
+        np.array([[1.6789373877365803, 0.0], [0.0, 2.577850090371836]]),
+        np.array([[0.6433491348614259, 0.0], [0.0, 1.1906381155257868]]),
+        np.array([[0.5752207318528584, 0.0], [0.0, 0.958701219754764]]),
+    ]
+    assert np.allclose(ntf_obj.jxz, expected_jxz)
 
 
 def test_samples_x1():
@@ -148,15 +148,14 @@ def test_samples_x1():
     ntf_obj = Nataf(distributions=[dist1, dist2])
     samples_x = np.array([[0.3, 1.2, 3.5], [0.2, 2.4, 0.9]]).T
     ntf_obj.run(samples_x=samples_x, jacobian=True)
-    g = []
-    for i in range(3):
-        if i == 0:
-            g.append((ntf_obj.samples_z[i] == np.array([-1.5547735945968535, -1.501085946044025])).all())
-        elif i == 1:
-            g.append((ntf_obj.samples_z[i] == np.array([-0.7063025628400874, 0.841621233572914])).all())
-        else:
-            g.append((ntf_obj.samples_z[i] == np.array([0.5244005127080407, -0.5244005127080409])).all())
-    assert np.all(g)
+    expected_samples_z = np.array(
+        [
+            [-1.5547735945968535, -1.501085946044025],
+            [-0.7063025628400874, 0.841621233572914],
+            [0.5244005127080407, -0.5244005127080409],
+        ]
+    )
+    assert np.allclose(ntf_obj.samples_z, expected_samples_z)
 
 
 def test_samples_z_jzx1():
@@ -173,13 +172,11 @@ def test_samples_z_jzx2():
     ntf_obj = Nataf(distributions=[dist1, dist2])
     samples_z = np.array([[0.3, 1.2], [0.2, 2.4]]).T
     ntf_obj.run(samples_z=samples_z, jacobian=True)
-    g = []
-    for i in range(2):
-        if i == 0:
-            g.append((ntf_obj.jzx[i] == np.array([[0.524400601939789, 0.0], [0.0, 0.8524218415758338]])).all())
-        else:
-            g.append((ntf_obj.jzx[i] == np.array([[1.0299400748281828, 0.0], [0.0, 14.884586948005541]])).all())
-    assert np.all(g)
+    expected_jzx = [
+        np.array([[0.524400601939789, 0.0], [0.0, 0.8524218415758338]]),
+        np.array([[1.0299400748281828, 0.0], [0.0, 14.884586948005541]]),
+    ]
+    assert np.allclose(ntf_obj.jzx, expected_jzx)
 
 
 def test_samples_z2():
@@ -188,13 +185,13 @@ def test_samples_z2():
     ntf_obj = Nataf(distributions=[dist1, dist2])
     samples_z = np.array([[0.3, 1.2], [0.2, 2.4]]).T
     ntf_obj.run(samples_z=samples_z, jacobian=True)
-    g = []
-    for i in range(2):
-        if i == 0:
-            g.append((ntf_obj.samples_x[i] == np.array([3.089557110944763, 1.737779128317309])).all())
-        elif i == 1:
-            g.append((ntf_obj.samples_x[i] == np.array([4.424651648891459, 2.9754073922262116])).all())
-    assert np.all(g)
+    expected_samples_x = np.array(
+        [
+            [3.089557110944763, 1.737779128317309],
+            [4.424651648891459, 2.9754073922262116],
+        ]
+    )
+    assert np.allclose(ntf_obj.samples_x, expected_samples_x)
 
 
 def test_itam_beta():
@@ -272,4 +269,4 @@ def distortion_z2x_dist_object():
     dist1 = Lognormal(s=0.0, loc=0.0, scale=1.0)
     rz = np.array([[1.0, 0.8], [0.8, 1.0]])
     with pytest.raises(Exception):
-        assert Nataf.distortion_z2x(distributions=[dist1, 'Beta'], corr_z=rz)
+        assert Nataf.distortion_z2x(distributions=[dist1, "Beta"], corr_z=rz)
